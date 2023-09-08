f13746e200e28b15372f7d82a0870641b2152785
Yonghun <yonghuni484@gmail.com>
2023-09-08 17:12:49 +0900

    Env: Add firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: "toy-1-1dfe1.firebaseapp.com",
+  projectId: "toy-1-1dfe1",
+  storageBucket: "toy-1-1dfe1.appspot.com",
+  messagingSenderId: "6124898264",
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: "G-GFE642HZGF",
+};
+
+const app = initializeApp(firebaseConfig);
