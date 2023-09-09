3546f831a80324fec1434c4002f912eb47f8e05e
gaori <monooi@naver.com>
2023-09-09 14:46:07 +0900

    feat: Add common flex container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <>
       <Header />
+      <SideBar />
       <Outlet />
     </>
   );
