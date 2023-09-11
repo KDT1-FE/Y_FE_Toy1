ecdb235f335e41db6089f14a31603b3217e8c1c4
wowba <leeyuwk54@gmail.com>
2023-09-11 12:02:19 +0900

    firebaseSDK 추가

---
+++ src/firebaseSDK.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAty4PjJBHIoWyvKNGwVRcaX1M-60iPfTE",
+  authDomain: "wikinity-947f0.firebaseapp.com",
+  projectId: "wikinity-947f0",
+  storageBucket: "wikinity-947f0.appspot.com",
+  messagingSenderId: "390893293038",
+  appId: "1:390893293038:web:02aa6ad55fb5c67283d2ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
