9c1dacb5ff93f30e9d699673f49b348a7d63e24f
Hojin <wkdghwls0623@gmail.com>
2023-09-11 18:47:53 +0900

    Feat:사이드바 출첵큐알코드, 아이콘 구현완료

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,41 +1,39 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import styled from 'styled-components'
-import "./Sidebar.css"
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-  const location = useLocation()
-  const hashSplit = location.pathname.split('/')
+  const location = useLocation();
+  const hashSplit = location.pathname.split("/");
 
-  if(hashSplit[1] === 'Wiki'){
-    const sideLink = ["", "교육과정"]
-    const sideName = ["행정", "교육과정"]
-    
-    // Wiki 사이드바 
+  if (hashSplit[1] === "Wiki") {
+    const sideLink = ["", "교육과정"];
+    const sideName = ["행정", "교육과정"];
+
+    // Wiki 사이드바
     return (
       <Container>
         <ul className="sidebar__link_wrapper">
-          {
-            sideLink.map((link,idx)=>
-              <li key={sideName[idx]} >
-                <Link to={`/Wiki/${link}`}> {sideName[idx]} </Link>
-              </li>  
-            )
-          }
+          {sideLink.map((link, idx) => (
+            <li key={sideName[idx]}>
+              <Link to={`/Wiki/${link}`}> {sideName[idx]} </Link>
+            </li>
+          ))}
         </ul>
+        <SidebarBottom />
       </Container>
-    )
+    );
 
     // Gallery 사이드바
-  }else if(hashSplit[1] === "Gallery"){
+  } else if (hashSplit[1] === "Gallery") {
     return (
       <Container>
-        Gallery Sidebar
+        <SidebarBottom />
       </Container>
-    )
+    );
   }
-  
-}
+};
 
 const Container = styled.aside`
   position: fixed;
@@ -46,7 +44,43 @@ const Container = styled.aside`
   width: 140px;
   height: 100%;
   background-color: #ddd;
-`
+`;
 
+function SidebarBottom(): JSX.Element {
+  return (
+    <>
+      <div className="sidebar__bottom">
+        <hr className="sidebar__bottom_hr" />
+        <a href="https://app.slack.com/client/T057XJP4T34/C05FRTBDHDL">
+          <img
+            className="sidebar__bottom_QRcode"
+            src={process.env.PUBLIC_URL + "/svg/AttendanceQR.svg"}
+            alt="QR코드"
+          />
+        </a>
+        <div className="sidebar__bottom_icon">
+          <a href="https://www.notion.so/X-24f85bf2ff4e4c69bd45ddc4e05d464b">
+            <img
+              src={process.env.PUBLIC_URL + "/svg/icon-notion.svg"}
+              alt="notion"
+            />
+          </a>
+          <a href="https://docs.google.com/spreadsheets/d/1Ffg--2TCzecwLMODoBHrTAk7zWufPLrJoyLVCwE4ea4/edit#gid=1823006152">
+            <img
+              src={process.env.PUBLIC_URL + "/svg/icon-sheet.svg"}
+              alt="sheet"
+            />
+          </a>
+          <a href="https://us06web.zoom.us/j/4912611157?pwd=N2swc3kxRG9uYTFKa2lBTUI2dS9NZz09#success">
+            <img
+              src={process.env.PUBLIC_URL + "/svg/icon-zoom.svg"}
+              alt="zoom"
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
 
-export default Sidebar
+export default Sidebar;
