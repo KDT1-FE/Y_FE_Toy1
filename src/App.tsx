5bcf05b6a2c47135cbea3d0992fede30bbfba9aa
89882 <pgh123166@naver.com>
2023-09-11 12:29:46 +0900

    feat: App.js -> Main.tsx migration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Main from 'pages/Main';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function Main() {
+  return '';
+}
+
+export default Main;
