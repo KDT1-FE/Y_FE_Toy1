4310c2aabac9c7a35b22cc337efeb1fff4c076dd
SKY-PEY <lovr8537@gmail.com>
2023-09-11 22:43:13 +0900

    [feat] gallery 컴포넌트 추가

---
+++ src/components/gallery/galleryMain.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export function New() {
+  return (
+    <div>
+      <h1>hello every one</h1>
+    </div>
+  );
+}
---
+++ src/components/gallery/gallerySwiper.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+// export interface IAppProps {
+// }
+
+// export function App (props: IAppProps) {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// }
