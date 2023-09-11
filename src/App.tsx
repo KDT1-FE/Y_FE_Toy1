9f8e1143c6bbae0913bcf81b97911294b08f5844
SOL-MI <ovo10203@gmail.com>
2023-09-11 23:02:32 +0900

    [feat] study페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Root from 'pages/Root';
 import { SignUp } from 'pages/SignUp';
+import { Study } from 'pages/Study';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Root />,
+      children: [{ path: '/study', element: <Study /> }],
     },
     {
       path: '/signup',
