ca428e4d5b25f054b28ebe87b91807d4070f979f
89882 <pgh123166@naver.com>
2023-09-11 16:42:38 +0900

    feat: add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from 'pages/Main';
+import Header from 'components/Header';
 
 function App() {
   return (
     <Router>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
