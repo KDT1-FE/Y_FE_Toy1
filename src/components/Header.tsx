0a10fd729959df149ea47435f2a202c4fdd30557
gaori <monooi@naver.com>
2023-09-11 12:25:35 +0900

    feat: Add search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
-import { AiFillEdit } from 'react-icons/ai';
+import { LuPenSquare } from 'react-icons/lu';
+import { CiSearch } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Container>
       <Navbar>
-        <input type="text" />
-      </Navbar>
-      <Navbar>
+        <InputWrapper>
+          <div className="icon-wrapper">
+            <CiSearch size="20" />
+          </div>
+          <input type="text" />
+        </InputWrapper>
         <Link to={'wiki'}>
-          <AiFillEdit size="24" />
+          <div className="icon-wrapper">
+            <LuPenSquare size="24" />
+          </div>
         </Link>
       </Navbar>
     </Container>
@@ -34,10 +40,46 @@ const Container = styled.header`
 
 const Navbar = styled.nav`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
+  width: 100%;
   height: 100%;
+
+  .icon-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    opacity: 0.5;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  width: 240px;
+
+  background-color: #f9f9f9;
+  border-radius: 20px;
+
+  input[type='text'] {
+    width: 100%;
+    padding: 0.8rem 1rem 0.8rem 0;
+    border: none;
+    outline: none;
+    background-color: transparent;
+
+    font-size: ${(props) => props.theme.fontSize.text};
+  }
 `;
 
 export default Header;
