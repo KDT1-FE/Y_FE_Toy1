d90d4c60d3e79eb8b1306322f58278e28a8fd0c5
89882 <pgh123166@naver.com>
2023-09-11 18:01:57 +0900

    style: seperate style code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,59 +7,6 @@ import { useNavigate } from 'react-router-dom';
 
 function Header() {
   const navigate = useNavigate();
-
-  const StyledHeader = styled.div`
-    height: 4rem;
-    box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
-  `;
-  const Container = styled.div`
-    max-width: 1580px;
-    margin: 0 auto;
-    height: 4rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    @media screen and (max-width: 1440px) {
-      max-width: 1240px;
-    }
-    @media screen and (max-width: 1280px) {
-      max-width: 1060px;
-    }
-  `;
-  const LogoContainer = styled.div`
-    width: 100px;
-    display: flex;
-    align-items: center;
-    font-weight: 700;
-    font-size: 1.5rem;
-    color: #3584f4;
-  `;
-  const MenuContainer = styled.div`
-    display: flex;
-    gap: 1rem;
-  `;
-  const Menu = styled.button`
-    font-size: 1.1rem;
-    font-weight: 300;
-    background-color: #fff;
-    border: none;
-    display: flex;
-    align-items: center;
-    gap: 0.3rem;
-    cursor: pointer;
-    &:hover {
-      border-bottom: 1px solid #4a5568;
-    }
-  `;
-  const CommuteMenu = styled(Menu)`
-    border: 1px solid #e2e8f0;
-    border-radius: 0.9rem;
-    padding: 0.5rem;
-    &:hover {
-      background-color: #edf2f7;
-      border-bottom: none;
-    }
-  `;
   return (
     <StyledHeader>
       <Container>
@@ -93,4 +40,56 @@ function Header() {
     </StyledHeader>
   );
 }
+const StyledHeader = styled.div`
+  height: 4rem;
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+`;
+const Container = styled.div`
+  max-width: 1580px;
+  margin: 0 auto;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media screen and (max-width: 1440px) {
+    max-width: 1240px;
+  }
+  @media screen and (max-width: 1280px) {
+    max-width: 1060px;
+  }
+`;
+const LogoContainer = styled.div`
+  width: 100px;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: #3584f4;
+`;
+const MenuContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+const Menu = styled.button`
+  font-size: 1.1rem;
+  font-weight: 300;
+  background-color: #fff;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 1px solid #4a5568;
+  }
+`;
+const CommuteMenu = styled(Menu)`
+  border: 1px solid #e2e8f0;
+  border-radius: 0.9rem;
+  padding: 0.5rem;
+  &:hover {
+    background-color: #edf2f7;
+    border-bottom: none;
+  }
+`;
 export default Header;
