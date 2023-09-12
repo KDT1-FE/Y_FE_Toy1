d39dd7317250944b23ecdb3ba0b58045c71d61d7
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-12 11:48:36 +0900

    Design: 인포 컴포넌트 스타일 추가

---
+++ src/pages/login/infoStyle.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+
+export const Info = styled.div`
+    width: 29rem;
+    height: 31.25rem;
+    background-color: rgba( 255, 255, 255, 0.05 );
+    border: 0.06rem solid #42537E;
+    border-radius: 0.94rem 0 0 0.94rem;
+    margin: 15rem 0 15rem 19.06rem;
+`;
+
+export const Logo = styled.div`
+
+`;
+
+export const CompanyName = styled.div`
+    color: var(--color-white);
+    font-size: 1.88rem;
+    line-height: normal;
+    text-align: left;
+    letter-spacing: -0.1rem;
+    margin: 11.69rem 0 0 5.38rem;
+`;
+
+
+export const ServiceName = styled.div`
+    color: var(--color-white); 
+    font-weight: bold;
+    font-size: 3.13rem;
+    line-height: normal;
+    text-align: left;
+    letter-spacing: -0.1rem;
+    margin: 0 17rem 11.75rem 5.38rem
+`;
+
+
