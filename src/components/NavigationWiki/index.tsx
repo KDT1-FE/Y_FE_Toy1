2f6fd62da19f24967b2132d0c3e757490b1b1fcb
applevalley <skatkd7571@naver.com>
2023-09-13 02:03:57 +0900

    #9 feat: add wiki category list info

--- src/components/NavigationWiki/index.tsx
+++ src/components/NavigationWiki/index.tsx
@@ -1,9 +1,29 @@
 import styled from 'styled-components';
 import NavigationContainer from 'components/NavigationContainer';
 function NavigationWiki() {
+
+  const initialCompanyInfo = [
+    { id: 1, text: '회사 내규', pathName: 'companyRule'},
+    { id: 2, text: '팀 소개', pathName: 'companyTeam'},
+    { id: 3, text: '조직도', pathName: 'companyOrganization'},
+  ]
+
+  const initialProjectInfo = [
+    { id: 1, text: '진행중인 프로젝트', pathName: 'projects'},
+    { id: 2, text: '예정된 프로젝트', pathName: 'projectsExpected'},
+    { id: 3, text: '완료된 프로젝트', pathName: 'projectsFinished'},
+  ]
+
+  const initialOnBoardingInfo = [
+    { id: 1, text: '신입사원 필독서', pathName: 'onBoardingDocs'},
+    { id: 2, text: '온보딩 주제', pathName: 'onBoardingSubjects'}
+  ]
+
   return (
     <Container>
-      <NavigationContainer></NavigationContainer>
+      <NavigationContainer>
+
+      </NavigationContainer>
       <h1>test</h1>
     </Container>
   )
