265ff554819941cbc0581cdeb2f8549b6d7b5214
89882 <pgh123166@naver.com>
2023-09-12 14:12:21 +0900

    fix: circular dependency fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,53 +4,9 @@ import wikiLogo from '../../assets/icons/WikiLogo.svg';
 import galleryLogo from '../../assets/icons/GalleryLogo.svg';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
-import Modal from 'components/Modal';
-
-export const Menu = styled.button`
-  font-size: 1.1rem;
-  font-weight: 300;
-  background-color: #fff;
-  border: none;
-  display: flex;
-  align-items: center;
-  gap: 0.3rem;
-  cursor: pointer;
-  &:hover {
-    border-bottom: 1px solid #4a5568;
-  }
-`;
+import Modal, { Menu } from 'components/Modal';
 
 function Header() {
-  const StyledHeader = styled.div`
-    height: 4rem;
-    box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
-  `;
-  const Container = styled.div`
-    max-width: 1580px;
-    margin: 0 auto;
-    height: 4rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    @media screen and (max-width: 1440px) {
-      max-width: 1240px;
-    }
-    @media screen and (max-width: 1280px) {
-      max-width: 1060px;
-    }
-  `;
-  const LogoContainer = styled.div`
-    width: 100px;
-    display: flex;
-    align-items: center;
-    font-weight: 700;
-    font-size: 1.5rem;
-    color: #3584f4;
-  `;
-  const MenuContainer = styled.div`
-    display: flex;
-    gap: 1rem;
-  `;
   const navigate = useNavigate();
   return (
     <StyledHeader>
@@ -83,4 +39,34 @@ function Header() {
   );
 }
 
+const StyledHeader = styled.div`
+  height: 4rem;
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+`;
+const Container = styled.div`
+  max-width: 1580px;
+  margin: 0 auto;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media screen and (max-width: 1440px) {
+    max-width: 1240px;
+  }
+  @media screen and (max-width: 1280px) {
+    max-width: 1060px;
+  }
+`;
+const LogoContainer = styled.div`
+  width: 100px;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: #3584f4;
+`;
+const MenuContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
 export default Header;
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,31 +1,9 @@
 import styled from 'styled-components';
 import commuteLogo from '../../assets/icons/CommuteLogo.svg';
-import { Menu } from 'components/Header';
 import { useState } from 'react';
 import ReactModal from 'react-modal';
 
 function Modal() {
-  const CommuteMenu = styled(Menu)`
-    border: 1px solid #e2e8f0;
-    border-radius: 0.9rem;
-    padding: 0.5rem;
-    &:hover {
-      background-color: #edf2f7;
-      border-bottom: none;
-    }
-  `;
-  const CustomModal = styled(ReactModal)`
-    &.modal {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      width: 548px;
-      height: 440px;
-      background-color: #fff;
-      transform: translate(-50%, -50%);
-      z-index: 1;
-    }
-  `;
   const [showModal, setShowModal] = useState(false);
   const handleOpenModal = () => {
     setShowModal(true);
@@ -51,4 +29,39 @@ function Modal() {
   );
 }
 
+export const Menu = styled.button`
+  font-size: 1.1rem;
+  font-weight: 300;
+  background-color: #fff;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 1px solid #4a5568;
+  }
+`;
+
+export const CommuteMenu = styled(Menu)`
+  border: 1px solid #e2e8f0;
+  border-radius: 0.9rem;
+  padding: 0.5rem;
+  &:hover {
+    background-color: #edf2f7;
+    border-bottom: none;
+  }
+`;
+const CustomModal = styled(ReactModal)`
+  &.modal {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 548px;
+    height: 440px;
+    background-color: #fff;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  }
+`;
 export default Modal;
