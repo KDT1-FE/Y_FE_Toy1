46e80e050146e2b8defaecfee154ef658cc9d5d0
joanShim <joanjeongahshim@gmail.com>
2023-09-13 02:32:11 +0900

    Feat: 모달 컴포넌트 정의

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import './Header.css'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Modal } from "./Modal";
+import "./Header.css";
 
 const Header = () => {
-  const pageLink = ["", "Wiki/", "Gallery"]
-  const pageName = ["Home", "Wiki", "Gallery"]
+  const pageLink = ["", "Wiki/", "Gallery"];
+  const pageName = ["Home", "Wiki", "Gallery"];
+
+  const [isModalActive, setIsModalActive] = useState(false);
+  const openModal = () => {
+    setIsModalActive(true);
+  };
 
   return (
-    <Container>
-      <ul className="header__link_wrapper">
-        {
-          pageLink.map((link,idx)=>
-            <li key={pageName[idx]} >
+    <>
+      <Container>
+        <ul className="header__link_wrapper">
+          {pageLink.map((link, idx) => (
+            <li key={pageName[idx]}>
               <Link to={`/${link}`}> {pageName[idx]} </Link>
-            </li>  
-          )
-        }
-      </ul>
-    </Container>
-  )
-}
+            </li>
+          ))}
+          <button onClick={openModal}>모달열기</button>
+        </ul>
+      </Container>
 
-const Container = styled.nav`
+      {isModalActive && (
+        <Modal
+          // modal={isModalActive}
+          setModal={setIsModalActive}
+          width="400"
+          height="200"
+          element={<div>Hello Modal!</div>}
+        />
+      )}
+    </>
+  );
+};
+
+export const Container = styled.nav`
   position: fixed;
   left: 0;
   top: 0;
@@ -32,6 +49,6 @@ const Container = styled.nav`
   background-color: #999;
   font-size: 1rem;
   z-index: 10;
-`
+`;
 
-export default Header
+export default Header;
