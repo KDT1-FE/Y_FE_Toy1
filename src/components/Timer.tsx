4ee1c78670add1e3fb7417ae520423d9345d0e2b
SOL-MI <ovo10203@gmail.com>
2023-09-12 21:03:16 +0900

    [style] type수정

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,43 +1,52 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 
-// export interface IAppProps {
-// }
-
 //unix timestamp
 const timeStamp = Math.floor(new Date().getTime() / 1000);
-function getCurrentDateFromStamp(timestamp: number) {
+function getCurrentDateFromStamp(timestamp: number): number {
   const fullDate = new Date(timestamp * 1000);
   const onlyDate = fullDate.getDate();
   return onlyDate;
 }
 
 //Timer의 시작, 중지, 초기화 동작
-function handleTimer(initialValue: number, ms: number) {
+function handleTimer(
+  initialValue: number,
+  ms: number,
+): {
+  count: number;
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+} {
   const [count, setCount] = useState(initialValue);
-  const intervalRef: any = useRef(null);
-  const start = useCallback(() => {
+  const intervalRef: React.MutableRefObject<unknown | null> = useRef(null);
+  console.log(intervalRef);
+  const start: () => void = useCallback(() => {
     if (intervalRef.current !== null) {
       return;
     }
     intervalRef.current = setInterval(() => {
       setCount((c) => c + 1);
     }, ms);
+    console.log(intervalRef.current);
   }, []);
-  const stop = useCallback(() => {
+  console.log(intervalRef);
+  console.log(intervalRef.current);
+  const stop: () => void = useCallback(() => {
     if (intervalRef.current === null) {
       return;
     }
-    clearInterval(intervalRef.current);
+    clearInterval(intervalRef.current as number);
     intervalRef.current = null;
   }, []);
-  const reset = useCallback(() => {
+  const reset: () => void = useCallback(() => {
     setCount(0);
   }, []);
   return { count, start, stop, reset };
 }
 
 //Timer 컴포넌트
-export function Timer(props: any) {
+export function Timer(): JSX.Element {
   const [currentHours, setCurrentHours] = useState(0);
   const [currentMinutes, setCurrentMinutes] = useState(0);
   const [currentSeconds, setCurrentSeconds] = useState(0);
