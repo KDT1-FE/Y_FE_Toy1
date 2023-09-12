a3bd6f85bcabffca1712dc0ad8a643caff58cbd0
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-12 11:28:49 +0900

    Design: 로그인페이지 레이아웃 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,52 @@
-import * as style from "./loginStyle";
+import React from "react";
+import { Container }from "./containerStyle";
+import { Info, Logo, CompanyName, ServiceName } from "./infoStyle";
+import { Content, Title, LoginBox }from "./contentStyle";
+import { Form, Id, Pw, IdInput, PwInput}from "./formStyle";
+import { ButtonSet,  Enter, Join  }from "./buttonStyle";
 
-export default function Login() {
+
+
+const LoginPage = () => {
   return (
-    <>
-      <style.Temp>Login</style.Temp>
-    </>
+    <Container>
+
+      <Info>
+        <Logo>
+          <CompanyName>9굴</CompanyName>
+          <ServiceName>WIKI</ServiceName>
+        </Logo>
+      </Info>
+
+
+      <Content>
+
+         <LoginBox>
+         
+         <Title>로그인</Title>
+
+          <Form>
+            <Id>ID
+              <IdInput 
+              />
+            </Id>
+            <Pw>PW
+              <PwInput 
+              type="password" 
+              />
+            </Pw>
+          </Form>
+
+        <ButtonSet>
+          <Enter>들어가기</Enter>
+          <Join>회원가입</Join>
+        </ButtonSet>
+
+        </LoginBox>
+      </Content>
+
+    </Container>
   );
-}
+};
+
+export default LoginPage;
