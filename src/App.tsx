8285091e98b134ef215c0d9dd22b611bbd8a4d86
wowba <leeyuwk54@gmail.com>
2023-09-12 14:38:11 +0900

    Feat: LoginPage 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,25 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Layout, Home, About, Dashboard, NoMatch } from "./pages/TestPage/Test"
-
-
+import Login from './pages/LoginPage/Login';
 
 function App() {
   return (
-    <div className="App">
-      안녕하세요 wikinity 입니다!
-      <div>
-        <Routes>
-          {/* Route는 아래처럼 중첩 라우팅이 가능합니다! */}
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            {/* Using path="*"" means "match anything", so this route
+    <Routes>
+      {/* *Route 기초* 추후 삭제 예정 */}
+      {/* Route는 아래처럼 중첩 라우팅이 가능합니다! */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-            <Route path="*" element={<NoMatch />} />
-          </Route>
-        </Routes>
-      </div>
-    </div>
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+      {/* *Route 기초* */}
+      <Route path='/login' element={<Login />} />
+    </Routes>
   );
 }
 
