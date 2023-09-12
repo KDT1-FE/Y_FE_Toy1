1834a04ec54d7dd1b49cb0c2cb87082d60569254
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-12 11:50:31 +0900

    Design: 버튼 컴포넌트 디자인 추가

---
+++ src/pages/login/buttonStyle.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ButtonSet = styled.div`
+`;
+
+export const Enter = styled.button`
+    border-radius: 0.25rem;
+    background: var(--color-white);
+    border: 0.06rem solid var(--color-main);
+    width: 15.88rem;
+    height: 2.5rem;
+    color: var(--color-main);
+
+    &:hover {
+        cursor: pointer;
+          }
+`;
+
+export const Join = styled.button`
+    border-radius: 0.25rem;
+    background: var(--color-white);
+    border: 0.06rem solid var(--color-medium-gray);
+    width: 15.88rem;
+    height: 2.5rem;
+    color:  var(--color-dark-gray);
+    margin-top: 0.31rem;
+
+    &:hover {
+    cursor: pointer;
+      }
+
+`;
+
