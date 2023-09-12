0c34b36ecb50b60c2e93b2d425d059a79114bfa4
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-12 11:46:48 +0900

    Design: 컨테이너 레이아웃 추가

---
+++ src/pages/login/containerStyle.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    background-color: var(--color-main);
+    justify-content:center;
+`;
