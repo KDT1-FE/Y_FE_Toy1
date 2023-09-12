4c617628bdf6bb51012bd02a8f681f9327858927
Taegwan Youn <tkyoun0421@naver.com>
2023-09-12 14:44:43 +0900

    Feat: add sideBar

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
+import '../scss/base/_index.scss';
 import SideBar from '../components/SideBar';
 
-export const LandingPage = () => {
+export const LandingPage = (): JSX.Element => {
   return (
     <div>
+      <Header />
       <SideBar />
     </div>
   );
