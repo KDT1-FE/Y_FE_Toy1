57d64f2a08ae49a6c17f8c742b78798d4fb918dd
89882 <pgh123166@naver.com>
2023-09-12 17:02:43 +0900

    style: remove import App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Main from 'pages/Main';
 import Gallery from 'pages/Gallery';
 import Header from 'components/Header';
