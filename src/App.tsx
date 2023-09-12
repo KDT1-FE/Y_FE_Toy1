48c8466389693da890ce7f5e7516905dac342665
turkey-kim <rlaxmrgml@naver.com>
2023-09-12 23:19:28 +0900

    feat: wiki 세부페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import "./styles/App.css";
 import "./styles/reset.css";
+import Wiki from "./pages/Wiki";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" />
-        <Route path="/wiki" />
+        <Route path="/wiki" element={<Wiki />}>
+          <Route path=":id" element={<Wiki />} />
+        </Route>
         <Route path="/gallery" />
       </Routes>
     </div>
