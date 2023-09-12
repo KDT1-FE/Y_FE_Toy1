c7755cf6701ac6cf287a294ba8f6d31355a4c1e6
yangjaehyuk <gndksakxk10@naver.com>
2023-09-12 17:53:03 +0900

    test: firebase env 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+// import {Routes, Route} from "react-router-dom";
 import "./styles/App.css";
 import "./styles/reset.css";
+import {collection, addDoc} from "firebase/firestore";
+import {ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
+import {db, storage} from "./utils/firebaseConfig";
 
 function App() {
+  async function createTestDoc() {
+    await addDoc(collection(db, "tests"), {
+      hello: "hello~",
+    });
+  }
+  function createTestImg(e: any) {
+    try {
+      const file = e.currentTarget.files?.[0];
+      const fileName = file.name;
+      const testImgsRef = ref(storage, `testImgs/${fileName}`);
+      const uploadTask = uploadBytesResumable(testImgsRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        snapshot => {
+          console.log(`1. snapshot:${snapshot}`);
+        },
+        err => {
+          console.log(`2. upload test Img err:${err}`);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+            console.log(`3. downloadURL:${downloadURL}`);
+          });
+        },
+      );
+    } catch (err) {
+      console.log(`0. createTestImg err: ${err}`);
+    }
+  }
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" />
-        <Route path="/wiki" />
-        <Route path="/gallery" />
-      </Routes>
+      <header className="App-header">
+        <button type="button" onClick={createTestDoc}>
+          create test doc
+        </button>
+        <input type="file" onChange={e => createTestImg(e)} />
+      </header>
     </div>
   );
 }
--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -3,13 +3,13 @@ import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXIwvd9be15vSnvd20ueJATT_JVMeeXSc",
-  authDomain: "techschool-wiki.firebaseapp.com",
-  projectId: "techschool-wiki",
-  storageBucket: "techschool-wiki.appspot.com",
-  messagingSenderId: "979662569232",
-  appId: "1:979662569232:web:13c25ea2c3eaf928242f95",
-  measurementId: "G-6YSVH86LFH",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTED_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
