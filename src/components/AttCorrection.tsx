23148ae1553551b66c2352027ad3fe284415e64c
hhjs2 <0092h@naver.com>
2023-09-12 13:08:45 +0900

    Feat : Wiki 수정기능 구현

---
+++ src/components/AttCorrection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AttCorrection = () => {
+  return <div>AttCorrection</div>;
+};
+
+export default AttCorrection;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,41 +1,48 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import styled from 'styled-components'
-import "./Sidebar.css"
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-  const location = useLocation()
-  const hashSplit = location.pathname.split('/')
+  const location = useLocation();
+  const hashSplit = location.pathname.split("/");
 
-  if(hashSplit[1] === 'Wiki'){
-    const sideLink = ["", "교육과정"]
-    const sideName = ["행정", "교육과정"]
-    
-    // Wiki 사이드바 
+  if (hashSplit[1] === "Wiki") {
+    //const sideLink = ["", "행정", "학습일정"]
+    const sideName = ["출석", "행정", "학습일정"];
+    const sideLinkAttendance = ["출석 인정", "QR출결 정정 프로세스"];
+    const sideLinkAdmin = ["휴가", "훈련장려금"];
+
+    // Wiki 사이드바
     return (
       <Container>
         <ul className="sidebar__link_wrapper">
-          {
-            sideLink.map((link,idx)=>
-              <li key={sideName[idx]} >
-                <Link to={`/Wiki/${link}`}> {sideName[idx]} </Link>
-              </li>  
-            )
-          }
+          <li key={"출석"}>
+            <Link to={`/Wiki/`}>출석</Link>
+          </li>
+          {sideLinkAttendance.map((link, idx) => (
+            <li key={sideLinkAttendance[idx]}>
+              <Link to={`/Wiki/${link}`}>{link}</Link>
+            </li>
+          ))}
+          <li key={"행정"}>행정</li>
+          {sideLinkAdmin.map((link, idx) => (
+            <li key={sideLinkAdmin[idx]}>
+              <Link to={`/Wiki/${link}`}> {link} </Link>
+            </li>
+          ))}
+          <li key={"학습 일정"}>
+            <Link to={`/Wiki/학습 일정`}>금주의 학습 일정</Link>
+          </li>
         </ul>
       </Container>
-    )
+    );
 
     // Gallery 사이드바
-  }else if(hashSplit[1] === "Gallery"){
-    return (
-      <Container>
-        Gallery Sidebar
-      </Container>
-    )
+  } else if (hashSplit[1] === "Gallery") {
+    return <Container>Gallery Sidebar</Container>;
   }
-  
-}
+};
 
 const Container = styled.aside`
   position: fixed;
@@ -46,7 +53,6 @@ const Container = styled.aside`
   width: 140px;
   height: 100%;
   background-color: #ddd;
-`
+`;
 
-
-export default Sidebar
+export default Sidebar;
