e0c151c82b9422b3fa557b06bb0992b4740155e4
Leejinuk <leejin7900@gmail.com>
2023-09-13 12:29:55 +0900

    feat Header, Footer, clock, Carousel, custom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import {Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./styles/Reset.css";
+import Carousel from "./components/Home/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carousel />
       <Routes>
         <Route path="/Home" />
         <Route path="/wiki" />
