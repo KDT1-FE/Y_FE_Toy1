8e730a50c5ce842158541cad4ffa72fb5963977a
minseokiim <alstj991226@naver.com>
2023-09-13 14:49:31 +0900

    Feat : use marked

--- src/pages/wiki/Team.tsx
+++ src/pages/wiki/Team.tsx
@@ -1,10 +1,44 @@
-import { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
+import { useState, useEffect } from 'react';
+import { marked } from 'marked';
 
 const Team = () => {
+  const initialMarkdown = localStorage.getItem('saved') || '소ㄱㅐ내용~~';
+  const [markdown, setMarkdown] = useState<string>(initialMarkdown);
+  const [tempMarkdown, setTempMarkdown] = useState<string>(markdown);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  useEffect(() => {
+    localStorage.setItem('saved', markdown);
+  }, [markdown]);
+
+  const handleEditClick = () => {
+    if (isEditing) {
+      setMarkdown(tempMarkdown);
+    } else {
+      setTempMarkdown(markdown);
+    }
+    setIsEditing(!isEditing);
+  };
+
+  const renderedMarkdown = marked(markdown);
+
   return (
     <div>
-      <button></button>
+      {isEditing ? (
+        <div>
+          <textarea
+            value={tempMarkdown}
+            onChange={(e) => setTempMarkdown(e.target.value)}
+            placeholder="내용을 입력하세요"
+          />
+        </div>
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: renderedMarkdown }} />
+      )}
+
+      <div>
+        <button onClick={handleEditClick}>{isEditing ? '저장' : '편집'}</button>
+      </div>
     </div>
   );
 };
