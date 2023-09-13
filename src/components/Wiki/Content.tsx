740c9923f32718d6e205f62a179e9eca144b215f
turkey-kim <rlaxmrgml@naver.com>
2023-09-13 12:11:20 +0900

    feat: Wiki 메인 페이지 디폴트 설정 - wiki 메인 페이지에서, 1번 리스트 렌더링으로 디폴트 설정

--- src/components/Wiki/Content.tsx
+++ src/components/Wiki/Content.tsx
@@ -13,23 +13,41 @@ function Content() {
     console.log(editTrue);
   };
 
+  const isIdExist = () => {
+    if (id) {
+      setText(id);
+    } else {
+      setText("회사생활");
+    }
+  };
+
   useEffect(() => {
-    console.log(id);
-    setText(id);
+    isIdExist();
     console.log(text);
   }, [id]);
 
   return (
-    <div style={{width: "80%", fontSize: "100%"}}>
-      <h1>{text}</h1>
-      <button type="button" onClick={clickEdit}>
+    <div className="WikiContentWrap">
+      <h1 id="ContentTitle">{text}</h1>
+      <button
+        className="WikiButton"
+        type="button"
+        onClick={clickEdit}
+        style={{
+          backgroundColor: editTrue ? "rgba(255, 55, 115, 0.8)" : "#34576d",
+        }}
+      >
         {editTrue ? "수정취소" : "수정하기"}
       </button>
       <div id="main-content">
         {editTrue ? (
           <TextEditor />
         ) : (
-          <div>Url 파라미터값을 키로 갖는 데이터를 가져와 렌더링해줍니다</div>
+          <div>
+            Url 파라미터값(현재값 : {text})을 Key로 갖는 데이터를 가져와
+            렌더링합니다, <br /> 렌더링 시에는 외부라이브러리를 통해
+            마크다운텍스트(데이터)를 html로 변환하여 렌더링합니다
+          </div>
         )}
       </div>
     </div>
