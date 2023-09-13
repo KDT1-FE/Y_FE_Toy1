459015658e8073bec8dd69f3ff64540ca274af2e
JinJeongMin <jdsjm2@naver.com>
2023-09-13 12:12:15 +0900

    Feat: carousel 기능 구현

---
+++ src/components/Slider/style.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const SliderItem = styled.div`
+    width: 1440px;
+    height: 400px;
+
+    margin: 0 auto;
+
+    background-color: #efefef;
+`;
+
+export const SliderImg = styled.img`
+    display: block;
+
+    width: 1440px;
+    height: 400px;
+`;
