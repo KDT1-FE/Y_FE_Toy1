926c6137b03e9b06ec8096f58213f088179f8138
turkey-kim <rlaxmrgml@naver.com>
2023-09-13 17:06:35 +0900

    fix: 캐러셀 렌더링 버그 수정 - 메인페이지에만 반영

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Carousel from "../components/Home/Carousel";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div style={{width: "100vw", height: "100vh"}}>
+      <Carousel />
+    </div>
+  );
 }
 
 export default Home;
