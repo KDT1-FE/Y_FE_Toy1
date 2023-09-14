b2bdd2322721898b622b53135da721bc0091cc74
furaha707 <zxcvyein950823@gmail.com>
2023-09-14 15:50:35 +0900

    Fix: firebase 버그수정 및 수정/삭제 기능 구현

--- src/pages/GalleryList.tsx
+++ src/pages/GalleryList.tsx
@@ -5,7 +5,48 @@ import { db } from "../firebase"
 import { collection, getDocs } from  "firebase/firestore"
 import { Link } from "react-router-dom";
 
-// 리스트 래퍼 스타일
+const GalleryList: React.FC = () => {
+  
+  // user의 문서정보 상태관리
+  const [users, setUsers] = useState<any[]>([]);
+  const usersCollectionRef = collection(db, "user");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      // 비동기로 user의 데이터 가져오기
+      const data = await getDocs(usersCollectionRef);
+      // 가져온 데이터 setUsers에 데이터 할당
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    };
+    getUsers();
+  }, []);
+  
+  return (
+    <>
+    <Link to="/gallery/edit">
+          <GalleryBtn type="button">새 글 작성</GalleryBtn>
+    </Link>
+    <ListWrapper>
+      {users.map((user) => {
+        return (
+          <Link to={`/gallery/detail/${user.id}`} key={user.id}>
+            <div  className="Gallery_link">
+              <p className="img-bx"><img src={user.thumbnail} alt="썸네일"/></p>
+              <p>{user.title}</p>
+              <p>
+                <span>{user.date} | </span>
+                <span>{user.writer}</span>
+              </p>
+            </div>
+          </Link>
+        )
+      })}
+      </ListWrapper>
+    </>
+  )
+}
+
+// 스타일
 const ListWrapper = styled.div`
   display:flex;
   flex-wrap:wrap;
@@ -42,46 +83,5 @@ const GalleryBtn = styled.button`
   padding: 10px 20px;
   border-radius:10px;
 `
-
-const GalleryList: React.FC = () => {
-  
-  // user의 문서정보 상태관리
-  const [users, setUsers] = useState<any[]>([]);
-  const usersCollectionRef = collection(db, "user");
-
-  // useEffect(() => {
-  //   const getUsers = async () => {
-  //     // 비동기로 user의 데이터 가져오기
-  //     const data = await getDocs(usersCollectionRef);
-  //     // 가져온 데이터 setUsers에 데이터 할당
-  //     setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-  //   };
-  //   getUsers();
-  // }, [usersCollectionRef]);
-  
-  return (
-    <>
-    <Link to="/gallery/edit">
-          <GalleryBtn type="button">새 글 작성</GalleryBtn>
-    </Link>
-    <ListWrapper>
-      {users.map((user) => {
-        return (
-          <Link to={`/gallery/detail/${user.id}`} key={user.id}>
-            <div  className="Gallery_link">
-              <p className="img-bx"><img src={user.thumbnail} alt=""/></p>
-              <p>{user.title}</p>
-              <p>
-                <span>{user.date} | </span>
-                <span>{user.writer}</span>
-              </p>
-            </div>
-          </Link>
-        )
-      })}
-      </ListWrapper>
-    </>
-  )
-}
 
 export default GalleryList
