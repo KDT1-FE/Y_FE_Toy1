b6e979f74dd1d2ec233bde9c9e4b9057e921d3b9
JitHoon <imessi0336@gmail.com>
2023-09-14 17:58:26 +0900

    refact: 카테고리 이름 변경

--- src/components/Wiki/WikiSidebar.tsx
+++ src/components/Wiki/WikiSidebar.tsx
@@ -5,23 +5,21 @@ import {Link} from "react-router-dom";
 function WikiSidebar() {
   return (
     <div className="WikiSidebar">
-      <span className="WikiCategory">회사생활</span>
+      <span className="WikiCategory">Tech Life</span>
       <div className="CategoryListWrapper">
-        <Link to="/wiki/회사내규">회사내규</Link>
-        <Link to="/wiki/팀소개">팀 소개</Link>
-        <Link to="/wiki/조직도">조직도</Link>
+        <Link to="/wiki/커리큘럼">커리큘럼</Link>
+        <Link to="/wiki/교육생소개<">교육생 소개</Link>
+        <Link to="/wiki/스터디구성">스터디 구성</Link>
       </div>
       <span className="WikiCategory">프로젝트</span>
       <div className="CategoryListWrapper">
-        <Link to="/wiki/진행중인프로젝트">진행중인 프로젝트</Link>
+        <Link to="/wiki/진행중인프로젝트">진행 중인 프로젝트</Link>
         <Link to="/wiki/완료된프로젝트">완료된 프로젝트</Link>
       </div>
       <span className="WikiCategory">정책</span>
       <div className="CategoryListWrapper">
-        <Link to="/wiki/휴가정책">휴가 정책</Link>
-        <Link to="/wiki/휴가신청">휴가 신청</Link>
+        <Link to="/wiki/휴가정책">휴가정책</Link>
         <Link to="/wiki/복리후생">복리후생</Link>
-        <Link to="/wiki/경비규정">경비 규정</Link>
       </div>
     </div>
   );
