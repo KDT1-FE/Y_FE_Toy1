e498e8cc29f172d24b9d960a28da48fa90409452
wowba <leeyuwk54@gmail.com>
2023-09-14 17:23:49 +0900

    Feat: 메인페이지 임시 로그아웃 버튼 추가

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSetRecoilState } from 'recoil'
+import userState from '../../recoil/atoms/userState'
 
 export default function Main() {
+
+  const navigate = useNavigate()
+  const setUserState = useSetRecoilState(userState)
+
+  const handleLogout = async () => {
+    await setUserState({
+      isLogin: false,
+      userInfo: {}
+    })
+    navigate("/login")
+  }
+
   return (
-    <div>Main</div>
+    <div>Main
+      <button type="button" onClick={handleLogout}>로그아웃</button>
+    </div>
   )
 }
