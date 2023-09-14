7303c41b6f87dcbcfdb38f52a3e2b1e92be223d9
yangjaejun <yang7134@gmail.com>
2023-09-14 15:58:07 +0900

    Feat: 위키 페이지 index 파일, 스타일 추가

--- src/pages/wiki/wikiStyle.ts
+++ src/pages/wiki/wikiStyle.ts
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
-export const Temp = styled.div``;
+export const WikiWrapper = styled.div`
+  background-color: var(--color-area);
+  padding: 1.25rem 5rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+`;
