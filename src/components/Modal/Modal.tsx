16372dd4d8b092d5db5229235aa5c98cd6c9a272
suyeonnnnnnn <suyeon9923@naver.com>
2023-09-14 17:49:14 +0900

    feat: useModal.tsx 커스텀 훅 생성

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../../styles/Modal.css";
+
+interface ModalType {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function Modal({isOpen, onClose, children}: ModalType) {
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter" || event.key === " ") {
+      onClose();
+    }
+  };
+
+  return isOpen ? (
+    <div
+      className="ModalBackdrop"
+      onClick={onClose}
+      onKeyDown={handleKeyDown}
+      role="button"
+      tabIndex={0}
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ModalContent"
+        role="presentation"
+      >
+        {children}
+      </div>
+    </div>
+  ) : null;
+}
+
+export {};
