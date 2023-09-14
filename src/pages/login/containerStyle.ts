53361c4d8832590bdfe4df2a304e97496a8d2b7a
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-14 16:24:13 +0900

    Remove: 사용하지 않는 파일 삭제

--- src/pages/login/containerStyle.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.section`
-    overflow: hidden;
-    display: flex;
-    background-color: var(--color-main);
-    justify-content:center;
-`;
