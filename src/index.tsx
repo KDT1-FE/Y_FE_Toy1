fde62b7ef95183aa69553da602cb4ef0742c6110
wowba <leeyuwk54@gmail.com>
2023-09-14 17:35:21 +0900

    Chore: 불필요한 파일 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import App from './App';
