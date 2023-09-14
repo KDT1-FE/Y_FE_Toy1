70d7c5ad2ac37a1cff4fa1260217e2c4782c785a
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-14 15:45:35 +0900

    Design: 로그인 페이지 레이아웃 업데이트

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,78 +1,24 @@
 import React from "react";
-import { useState, useCallback } from "react";
-import Modal from "./SignUp";
+import { auth } from "../../../firebase";
+import SignIn from "@components/login/SignIn";
 
-import { ModalTitle, JoinBox } from "./modalStyle";
-import { Container } from "./containerStyle";
-import { Info, Logo, CompanyName, ServiceName } from "./infoStyle";
-import { Content, Title, LoginBox } from "./contentStyle";
-import { Form, InputWrap, Input } from "./formStyle";
-import { ButtonSet, HighlightButton, NormalButton } from "./buttonStyle";
+import * as style from "./loginStyle";
 
+console.log(auth);
 const LoginPage = () => {
-  const [isOpenModal, setOpenModal] = useState<boolean>(false);
-
-  const onClickToggleModal = useCallback(() => {
-    setOpenModal(!isOpenModal);
-  }, [isOpenModal]);
-
   return (
-    <Container>
-      {isOpenModal && (
-        <Modal onClickToggleModal={onClickToggleModal}>
-          <JoinBox>
-            <ModalTitle>회원가입</ModalTitle>
-            <Form>
-              <InputWrap className="IdWrap">
-                ID
-                <Input className="IdInput" />
-              </InputWrap>
+    <style.Container>
+      <style.Info>
+        <style.Logo>
+          <style.CompanyName>9굴</style.CompanyName>
+          <style.ServiceName>WIKI</style.ServiceName>
+        </style.Logo>
+      </style.Info>
 
-              <InputWrap className="IdWrap">
-                PW
-                <Input className="PWInput" />
-              </InputWrap>
-
-              <InputWrap className="NameWrap">
-                이름
-                <Input className="NameInput" />
-              </InputWrap>
-            </Form>
-            <HighlightButton> 가입하기</HighlightButton>
-          </JoinBox>
-        </Modal>
-      )}
-
-      <Info>
-        <Logo>
-          <CompanyName>9굴</CompanyName>
-          <ServiceName>WIKI</ServiceName>
-        </Logo>
-      </Info>
-
-      <Content>
-        <LoginBox>
-          <Title>로그인</Title>
-
-          <Form>
-            <InputWrap className="IdWrap">
-              ID
-              <Input className="IdInput" />
-            </InputWrap>
-
-            <InputWrap className="PwWrap">
-              PW
-              <Input className="PwInput" type="password" />
-            </InputWrap>
-          </Form>
-
-          <ButtonSet>
-            <HighlightButton> 들어가기</HighlightButton>
-            <NormalButton onClick={onClickToggleModal}>회원가입</NormalButton>
-          </ButtonSet>
-        </LoginBox>
-      </Content>
-    </Container>
+      <style.Content>
+        <SignIn />
+      </style.Content>
+    </style.Container>
   );
 };
 
