b3f919bdaf709f4d2ededa80ac4e7a86391147ab
dbstjrals <soekmin1998@naver.com>
2023-09-14 19:40:02 +0900

    feat: Add project state
    ongoing|scheduled|completed

--- src/pages/Gallery/Project.tsx
+++ src/pages/Gallery/Project.tsx
@@ -1,47 +1,59 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import galleryDummyImage from '../../assets/galleryDummyImage.webp';
 import ImageWrapper from './ImageWrapper';
 
-const Project = () => {
+export interface ProjectStateProps {
+  state: string;
+}
+
+const Project = ({ state }: ProjectStateProps) => {
+  let projectState: string =
+    state === 'ongoing'
+      ? '진행중인'
+      : state === 'scheduled'
+      ? '예정된'
+      : state === 'completed'
+      ? '종료된'
+      : '';
+
   return (
     <GalleryMainContainer>
       <CategoryTitleSection>
-        <h1>프로젝트</h1>
-        <BreadCrumb>갤러리 &gt; 사진첩 &gt; 프로젝트</BreadCrumb>
+        <h1>{projectState} 프로젝트</h1>
+        <BreadCrumb>갤러리 &gt; 프로젝트 &gt; {projectState} 프로젝트</BreadCrumb>
       </CategoryTitleSection>
       <ImageSection>
-        <ImageWrapper imageUrl={galleryDummyImage} />
-        <ImageWrapper imageUrl={galleryDummyImage} />
-        <ImageWrapper imageUrl={galleryDummyImage} />
-        <ImageWrapper imageUrl={galleryDummyImage} />
-        <ImageWrapper imageUrl={galleryDummyImage} />
-        <ImageWrapper imageUrl={galleryDummyImage} />
-        <ImageWrapper imageUrl={galleryDummyImage} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={1} state={state} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={2} state={state} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={3} state={state} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={4} state={state} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={5} state={state} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={6} state={state} />
+        <ImageWrapper imageUrl={galleryDummyImage} projectId={7} state={state} />
       </ImageSection>
-      {/* <h2>{category}</h2> */}
     </GalleryMainContainer>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
 
-const GalleryMainContainer = styled.div`
+export const GalleryMainContainer = styled.div`
   width: 100%;
   padding: 10px 30px 30px;
 `;
 
-const CategoryTitleSection = styled.section`
+export const CategoryTitleSection = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-const BreadCrumb = styled.span`
+export const BreadCrumb = styled.span`
   font-size: 12px;
   text-align: right;
   color: gray;
 `;
-const ImageSection = styled.section`
+export const ImageSection = styled.section`
   display: flex;
   flex-wrap: wrap;
   gap: 0 5%;
