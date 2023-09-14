b038fe3a4c82cc84112e0fbe31c69dd432f4b86c
KSJ <https://github.com/KSJT>
2023-09-14 22:44:31 +0900

    Feat: 홈 화면 갤러리 미리보기 추가, Docs: 이미지 파일 추가, Design: 홈 화면 레이아웃 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,43 @@
 import * as style from "./homeStyle";
+import styled from 'styled-components';
+import Carousel from './components/Carousel';
+import GalleryPreview from './components/GalleryPreview';
+
 
 export default function Home() {
   return (
     <>
-      <style.Temp>Home</style.Temp>
+      <style.Temp>
+          <div className="item1">
+            <h3>공지사항</h3>
+          </div>
+
+          <div className="item2">
+            <img style={{width: '55.63rem', height: '44.75rem'}} src="/images/modal_health_care.svg" alt="" />
+          </div>
+
+          <div className="item3">
+            <h3>최근 업데이트 된 WIKI</h3>
+          </div>
+
+          <div className="item4">
+            <img style={{width: '22.5rem', height: '12.5rem'}} src="/images/modal_workshop.svg" alt="" />
+          </div>
+
+          <div className="item5">
+            <img style={{width: '22.5rem', height: '12.5rem'}} src="/images/modal_reminder.svg" alt="" />
+          </div>
+
+          <div className="item6">
+            <h3>최근 업데이트 된 GALLERY</h3>
+          </div>
+
+          <div className="item7">
+            <GalleryPreview />
+          </div>
+          
+      </style.Temp>
+      <Carousel />
     </>
   );
 }
