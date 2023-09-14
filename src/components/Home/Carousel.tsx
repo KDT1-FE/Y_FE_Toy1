8dbdbcf46a2b9d5e26151dbb3c874c679b2a6766
Leejinuk <leejin7900@gmail.com>
2023-09-14 17:45:12 +0900

    feat: slide추가, fix: footer수정

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -1,17 +1,25 @@
 import React, {useState, useEffect} from "react";
 import "../../styles/Carousel.css";
-import img1 from "../../assets/slide1.jpg";
-import img2 from "../../assets/slide2.jpg";
+import img1 from "../../assets/slide01.jpg";
+import img2 from "../../assets/slide02.jpg";
+import img3 from "../../assets/slide03.jpg";
+import img4 from "../../assets/slide04.jpg";
+import img5 from "../../assets/slide05.jpg";
+import img6 from "../../assets/slide06.jpg";
+import img7 from "../../assets/slide07.jpg";
+import img8 from "../../assets/slide08.jpg";
+import img9 from "../../assets/slide09.jpg";
+import img10 from "../../assets/slide10.jpg";
 
 function Carousel() {
-  const images = [img1, img2];
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide(prevSlide => (prevSlide + 1) % images.length);
-    }, 4000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [images.length]);
