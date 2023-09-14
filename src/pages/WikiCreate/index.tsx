fbbbdbf7c16db9efc81a0932d00ec9a634117a79
applevalley <skatkd7571@naver.com>
2023-09-14 17:43:09 +0900

    feat: add write and preview toggle feature

--- src/pages/WikiCreate/index.tsx
+++ src/pages/WikiCreate/index.tsx
@@ -1,19 +1,43 @@
 import NavigationWiki from 'components/NavigationWiki';
 import styled from 'styled-components';
+import MDEditor from '@uiw/react-md-editor';
+import React, { useState } from 'react'
 
 function WikiCreate() {
+  const [textValue, setTextValue] = useState('')
+  const [writable, setWriteToggle] = useState(false);
+  
+  const handleSetValue = (e: string) => {
+    setTextValue(e);
+  };
+
+  const writeMode = () => {
+    setWriteToggle(() => false)
+  }
+  const previewMode = () => {
+    setWriteToggle(() => true)
+  }
+
   return (
   <WikiContainer>
     <NavigationWiki></NavigationWiki>
     <TextareaContainer>
       <ButtonContainer>
         <div>
-          <button>Write</button><button>Preview</button>
+          <button onClick={writeMode}>Write</button><button onClick={previewMode}>Preview</button>
         </div>
         <button>등록하기</button>
       </ButtonContainer>
-      <textarea placeholder='내용을 입력하세요.'></textarea>
-    </TextareaContainer>
+      {
+        writable === false ? 
+        <textarea 
+          placeholder='내용을 입력하세요.'
+          value={textValue}
+          onChange={(e) => {handleSetValue(e.target.value)}}
+        ></textarea> :
+        <MDEditor.Markdown source={textValue} />
+      }
+      </TextareaContainer>
   </WikiContainer>
   )
 }
