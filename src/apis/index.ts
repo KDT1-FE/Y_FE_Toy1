afe94ed3dcf9dc28dc99028d067d4da9c6b96606
89882 <pgh123166@naver.com>
2023-09-16 00:35:55 +0900

    feat: get workTime data

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -5,13 +5,24 @@ import {
   signInWithPopup,
 } from 'firebase/auth';
 import { auth, db } from './firebase';
-import { addDoc, collection, serverTimestamp } from '@firebase/firestore';
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+  where,
+} from '@firebase/firestore';
 import { WORK_TIME_COLLECTION } from 'constants/collection';
 
 export const login = async () => {
-  const provider = new GoogleAuthProvider();
-  await setPersistence(auth, browserSessionPersistence);
-  await signInWithPopup(auth, provider);
+  try {
+    const provider = new GoogleAuthProvider();
+    await setPersistence(auth, browserSessionPersistence);
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    alert('알 수 없는 오류입니다');
+  }
 };
 
 const getSessionUserData = () => {
@@ -23,17 +34,51 @@ const getSessionUserData = () => {
   }
 };
 
-interface userData {
+interface IuserData {
   [key: string]: unknown;
   uid: string;
+  displayName: string;
 }
-export const addWorkTimeData = async (workTime: number) => {
-  const user: userData = getSessionUserData();
+export const addWorkTimeData = (workTime: number) => {
+  const user: IuserData = getSessionUserData();
   try {
-    await addDoc(collection(db, WORK_TIME_COLLECTION), {
+    addDoc(collection(db, WORK_TIME_COLLECTION), {
       workTime: workTime,
       uid: user.uid,
       timeStamp: serverTimestamp(),
+      name: user.displayName,
+    });
+  } catch (error) {
+    alert('알 수 없는 오류입니다');
+  }
+};
+
+interface IresponseArray {
+  uid: string;
+  timeStamp: string;
+  workTime: number;
+  name: string;
+}
+const searchUser = () => {
+  const user: IuserData = getSessionUserData();
+  const searchUserQuery = query(
+    collection(db, WORK_TIME_COLLECTION),
+    where('uid', '==', user.uid),
+  );
+  return searchUserQuery;
+};
+export const getWorkTimeData = async () => {
+  try {
+    const response = await getDocs(searchUser());
+    const responseArray: IresponseArray[] = [];
+    response.forEach((doc) => {
+      const workTimeData = doc.data();
+      responseArray.push({
+        uid: workTimeData.uid,
+        timeStamp: workTimeData.timeStamp.toDate(),
+        workTime: workTimeData.workTime,
+        name: workTimeData.name,
+      });
     });
   } catch (error) {
     alert('알 수 없는 오류입니다');
