faabf9bb38e475a6bd9dc106b816ed634ecd3607
TaePoong719 <just5538@gmail.com>
2023-09-15 17:25:13 +0900

    Feat: 프로필 이미지 기능 추가

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,19 +1,79 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
 import { User } from "@firebase/auth";
+import { storage } from '../firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { updateProfile } from "firebase/auth"
 
 const UserInfo:React.FC<Props> = ({handlerLogout, user}) => {
   const [isLogout, setIsLogout] = useState(true) // Logout 모드(true) 또는 사진 추가 모드(false)가 가능합니다.
-  const userPhotoURL = user?.photoURL
+  const [userPhotoURL, setUserPhotoURL] = useState(user?.photoURL)
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const reader = new FileReader()
 
-  const handlerConfirmImage = () => {
-  
+  const handlerConfirmImage = async () => {
+    if(fileInputRef?.current){
+      const file = fileInputRef.current.files![0]
+      const filename = user.uid + Date.now()
+      const imageRef = ref(storage, `userImage/${filename}`)
+      try {
+        const snapshot = await uploadBytes(imageRef, file);
+        const url = await getDownloadURL(snapshot.ref);
+        setUserPhotoURL(url)
+        await updateProfile  (user, {
+          photoURL: url
+        })
+        alert('등록 성공했습니다')
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        // 이미지 업로드 실패 처리
+      }
+    }
+    
     setIsLogout(true)
+  }
+
+  const handleEditImage = () => {
+    if(fileInputRef?.current){
+      fileInputRef.current.click()
+      setIsLogout(false)
+    }
+  }
+
+  // 파일을 받을 경우
+  const handleFileChange = () => {
+    let file = null
+    if(fileInputRef?.current){
+      // 미리보기로 img src를 변경해줌
+      file = fileInputRef.current.files![0]
+      reader.onload = (e) => {
+        const base64DataUrl = e.target!.result as string
+        setUserPhotoURL(base64DataUrl)
+      }
+      if(file){
+        reader.readAsDataURL(file) 
+      }else{
+        setIsLogout(true)
+      }
+    }
   }
 
   return (
     <Container>
-      <img className="userInfo__user-img" src={userPhotoURL!} alt="유저 이미지" />
+      <div className="userInfo__img-container">
+        <img className="userInfo__user-img" src={userPhotoURL!} alt="유저 이미지" />
+        <div className="userInfo__img-edit" onClick={handleEditImage}>
+          
+          <img src="svg/icon-edit.svg" alt="수정버튼" />
+        </div>
+        <FileInput
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          />
+      </div>
+      
       <h1>{user.displayName+'님'}</h1>
       <h2>{user.email}</h2>
       
@@ -29,6 +89,10 @@ interface Props {
   user: User
 }
 
+const FileInput = styled.input`
+  display: none;
+`;
+
 const Container = styled.section`
   display: flex;
   flex-flow: column;
@@ -40,11 +104,23 @@ const Container = styled.section`
   box-sizing: border-box;
   padding: 35px 0;
   gap: 20px;
+  .userInfo__img-container{
+    position:relative;
+  }
   .userInfo__user-img{
     border-radius: 50%;
     height: 100px;
     width: 100px;
     box-sizing: border-box;
+  }
+  .userInfo__img-edit{
+    position: absolute;
+    border-radius: 50%;
+    background-color: white;
+    padding: 5px;
+    bottom:0;
+    right: 0;
+    cursor: pointer;
   }
   h1{
     margin: 0;
