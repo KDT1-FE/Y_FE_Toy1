fb08d4ed9ef9f3bd1cdd7e18c9d4b7143039c2d4
NamgungJongMin <jmnamgung@gmail.com>
2023-09-15 17:14:45 +0900

    Chore: "Modify changeAuthPath's style

--- src/components/Auth/ChangeAuthPage.tsx
+++ src/components/Auth/ChangeAuthPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../../scss/components/auth/changeAuthPage.scss';
+
 export const ChangeAuthPage = ({ target }): JSX.Element => {
   const checkTarget = (target: string) => {
     return <Link to={target}>{target === '/resister' ? 'Sign Up' : 'Sign In'}</Link>;
