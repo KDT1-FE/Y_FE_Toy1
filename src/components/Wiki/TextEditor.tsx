25bf4d44c482e9afed94c9f5ca8b0b4f12d42cff
JitHoon <imessi0336@gmail.com>
2023-09-16 20:25:51 +0900

    refact: 추론이 되는 state 타입 제거

--- src/components/Wiki/TextEditor.tsx
+++ src/components/Wiki/TextEditor.tsx
@@ -4,7 +4,7 @@ import {TextEditorProps} from "../../types/Wiki";
 import PostWiki from "./PostWiki";
 
 function TextEditor({dataKey, content, setIsEditorOpen}: TextEditorProps) {
-  const [text, setText] = useState<string>(content);
+  const [text, setText] = useState(content);
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setText(e.target.value);
