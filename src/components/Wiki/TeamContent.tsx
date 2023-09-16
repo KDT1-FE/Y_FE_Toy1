6889d150c2dedbbddc37a542c2b819cb43be7b69
JitHoon <imessi0336@gmail.com>
2023-09-16 20:27:19 +0900

    feat: 13개 팀 카드 컴포넌트 생성

--- src/components/Wiki/TeamContent.tsx
+++ src/components/Wiki/TeamContent.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SaveTeam from "./SaveTeam";
+import TeamCard from "./TeamCard";
 
 function TeamContent() {
   const sessionLength = sessionStorage.length;
   const teamListString = sessionStorage.getItem("teamList");
   let teamList = [];
-  let team = {멘티: "", 멘토: "", img: ""};
 
   if (!sessionLength) {
     SaveTeam();
@@ -15,19 +15,13 @@ function TeamContent() {
     ? (teamList = JSON.parse(teamListString).teamName)
     : SaveTeam();
 
-  return teamList.map((teamName: string) => {
-    const teamString = sessionStorage.getItem(teamName);
-    teamString ? (team = JSON.parse(teamString)) : SaveTeam();
-
-    return (
-      <div className="TeamContent">
-        <img className="TeamImg" src={team.img} alt="팀 이미지" />
-        <div className="Mentor">{team.멘티}</div>
-        <div className="Mentee">{team.멘토}</div>
-        {team.멘토}
-      </div>
-    );
-  });
+  return (
+    <div className="TeamContent">
+      {teamList.map((teamName: string) => (
+        <TeamCard teamName={teamName} />
+      ))}
+    </div>
+  );
 }
 
 export default TeamContent;
