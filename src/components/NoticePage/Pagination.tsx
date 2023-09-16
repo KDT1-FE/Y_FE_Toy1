26d9f0b9f9cafa85d9ef516d213bf47c5c3992b5
JiHongkyu <hongkyu9941@gmail.com>
2023-09-16 13:20:25 +0900

    Feat: 페이지네이션 구현

---
+++ src/components/NoticePage/Pagination.tsx
@@ -0,0 +1,23 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import * as S from '../../styled/NoticePage/Pagination.styles';
+
+interface IPaginationProps {
+  totalPages: number;
+  setPage: Dispatch<SetStateAction<number>>;
+}
+
+function Pagination({ totalPages, setPage }: IPaginationProps) {
+  return (
+    <S.Wrapper>
+      {new Array(totalPages).fill(1).map((_, index) => (
+        <S.Page key={index}>
+          <S.PageBtn type='button' onClick={() => setPage(index + 1)}>
+            {index + 1}
+          </S.PageBtn>
+        </S.Page>
+      ))}
+    </S.Wrapper>
+  );
+}
+
+export default Pagination;
--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
-import { useRecoilValue } from "recoil";
-import { Outlet, useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-import userState from "../recoil/atoms/userState";
-import Navigation from "./NavBar";
+import userState from '../recoil/atoms/userState';
+import Navigation from './NavBar';
 
 function LoginCheckRoute() {
-  const navigate = useNavigate()
-  const user = useRecoilValue(userState)
+  const navigate = useNavigate();
+  const user = useRecoilValue(userState);
 
   useEffect(() => {
     if (!user.isLogin) {
-      navigate("/login")
+      navigate('/login');
     }
-  }, [])
+  }, []);
 
-  return user.isLogin !== true ?
-    null :
+  return user.isLogin !== true ? null : (
     <>
       <Navigation />
       <Outlet />
     </>
+  );
 }
 
-export default LoginCheckRoute
+export default LoginCheckRoute;
