4790d6acd8ebf708bf585c36d13d0cfe5ac87311
turkey-kim <rlaxmrgml@naver.com>
2023-09-17 00:17:37 +0900

    feat: 로딩UI 추가

---
+++ src/components/Common/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/Loading.css";
+
+function Loading() {
+  return (
+    <div className="LoadingContainer">
+      <div className="LoadingCircle" />
+    </div>
+  );
+}
+
+export default Loading;
