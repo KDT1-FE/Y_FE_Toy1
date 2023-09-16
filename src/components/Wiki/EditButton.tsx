75f4919ef0a39f52bd8119fe704decbbcd8841c7
turkey-kim <rlaxmrgml@naver.com>
2023-09-16 12:46:02 +0900

    feat: 글수정 버튼 아이콘으로 대체

--- src/components/Wiki/EditButton.tsx
+++ src/components/Wiki/EditButton.tsx
@@ -1,24 +1,14 @@
 import React from "react";
 import "../../styles/Wiki.css";
 import {EditButtonProps} from "../../types/Wiki";
+import {ReactComponent as Icon} from "../../assets/icons/EditBtn.svg";
 
 function EditButton({isEditorOpen, setIsEditorOpen}: EditButtonProps) {
   const clickEdit = () => {
     setIsEditorOpen(!isEditorOpen);
   };
 
-  return (
-    <button
-      className="WikiButton"
-      type="button"
-      onClick={clickEdit}
-      style={{
-        backgroundColor: isEditorOpen ? "rgba(255, 55, 115, 0.8)" : "#34576d",
-      }}
-    >
-      {isEditorOpen ? "수정취소" : "수정하기"}
-    </button>
-  );
+  return <Icon className="EditButton" onClick={clickEdit} />;
 }
 
 export default EditButton;
