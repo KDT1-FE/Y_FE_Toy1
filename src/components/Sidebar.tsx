223f5138007401c1f8e5aa847c875ae8b292f1f0
Hojin <wkdghwls0623@gmail.com>
2023-09-17 21:15:27 +0900

    Feat: 캐러셀 구현완료

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
-import "./Sidebar.css";
 
 const Sidebar = () => {
   const location = useLocation();
   const hashSplit = location.pathname.split("/");
 
-  if (hashSplit[1] === "Wiki") {
+  if (hashSplit[1] === "Wiki" || "home") {
     //const sideLink = ["", "행정", "학습일정"]
     const sideName = ["출석", "행정", "학습일정"];
     const sideLinkAttendance = ["출석 인정", "QR출결 정정 프로세스"];
@@ -16,7 +15,7 @@ const Sidebar = () => {
     // Wiki 사이드바
     return (
       <Container>
-        <ul className="sidebar__link-wrapper">
+        <SidebarList>
           <li key={"출석"}>
             <Link to={`/Wiki/`}>출석</Link>
           </li>
@@ -34,7 +33,7 @@ const Sidebar = () => {
           <li key={"학습 일정"}>
             <Link to={`/Wiki/학습 일정`}>금주의 학습 일정</Link>
           </li>
-        </ul>
+        </SidebarList>
         <SidebarBottom />
       </Container>
     );
@@ -43,16 +42,30 @@ const Sidebar = () => {
   } else if (hashSplit[1] === "Gallery") {
     return (
       <Container>
-        <ul className="sidebar__link-wrapper">
+        <SidebarList>
           <li>갤러리글 1</li>
           <li>갤러리글 2</li>
           <li>갤러리글 3</li>
-        </ul>
+        </SidebarList>
         <SidebarBottom />
       </Container>
     );
   }
 };
+
+const SidebarList = styled.ul`
+  height: calc(100vh - 300px);
+  min-height: 200px;
+  overflow-y: auto;
+  display: flex;
+  flex-flow: column;
+  gap: 20px;
+`;
+
+const SidebarBottomBox = styled.div`
+  bottom: 0;
+  text-align: center;
+`;
 
 const Container = styled.aside`
   position: fixed;
@@ -62,41 +75,51 @@ const Container = styled.aside`
   background-color: #ddd;
 `;
 
-function SidebarBottom(): JSX.Element {
+const SidebarBottom = (): JSX.Element => {
   return (
-    <>
-      <div className="sidebar__bottom">
-        <hr className="sidebar__bottom-hr" />
-        <a href="https://app.slack.com/client/T057XJP4T34/C05FRTBDHDL">
+    <SidebarBottomBox>
+      <hr />
+      <a
+        href="https://app.slack.com/client/T057XJP4T34/C05FRTBDHDL"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/svg/AttendanceQR.svg"}
+          alt="QR코드"
+        />
+      </a>
+      <div>
+        <a
+          href="https://www.notion.so/X-24f85bf2ff4e4c69bd45ddc4e05d464b"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
-            className="sidebar__bottom_QRcode"
-            src={process.env.PUBLIC_URL + "/svg/AttendanceQR.svg"}
-            alt="QR코드"
+            src={process.env.PUBLIC_URL + "/svg/notion_icon.svg"}
+            alt="notion"
           />
         </a>
-        <div className="sidebar__bottom_icon">
-          <a href="https://www.notion.so/X-24f85bf2ff4e4c69bd45ddc4e05d464b">
-            <img
-              src={process.env.PUBLIC_URL + "/svg/icon-notion.svg"}
-              alt="notion"
-            />
-          </a>
-          <a href="https://docs.google.com/spreadsheets/d/1Ffg--2TCzecwLMODoBHrTAk7zWufPLrJoyLVCwE4ea4/edit#gid=1823006152">
-            <img
-              src={process.env.PUBLIC_URL + "/svg/icon-sheet.svg"}
-              alt="sheet"
-            />
-          </a>
-          <a href="https://us06web.zoom.us/j/4912611157?pwd=N2swc3kxRG9uYTFKa2lBTUI2dS9NZz09#success">
-            <img
-              src={process.env.PUBLIC_URL + "/svg/icon-zoom.svg"}
-              alt="zoom"
-            />
-          </a>
-        </div>
+        <a
+          href="https://docs.google.com/spreadsheets/d/1Ffg--2TCzecwLMODoBHrTAk7zWufPLrJoyLVCwE4ea4/edit#gid=1823006152"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={process.env.PUBLIC_URL + "/svg/sheet_icon.svg"}
+            alt="sheet"
+          />
+        </a>
+        <a
+          href="https://us06web.zoom.us/j/4912611157?pwd=N2swc3kxRG9uYTFKa2lBTUI2dS9NZz09#success"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={process.env.PUBLIC_URL + "/svg/zoom_icon.svg"} alt="zoom" />
+        </a>
       </div>
-    </>
+    </SidebarBottomBox>
   );
-}
+};
 
 export default Sidebar;
