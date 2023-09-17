8cec6928d932d30c5d36c8b9bdd57b4240a5ddba
wowba <leeyuwk54@gmail.com>
2023-09-17 22:26:15 +0900

    Feat: 상단 프로필 모달 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { NavBar, NavCategoryBox, NavCategoryLink, NavModalBox, NavTitle } from '../styled/Common/NavBar';
 import Calendar from '../assets/img/Calendar.svg';
 import Todo from '../assets/img/Todo.svg';
 import Profile from '../assets/img/Profile.svg';
+import ProfileModal from '../pages/MainPage/ProfileModal';
+import { ProfileBtn } from '../styled/MainPage/ProfileModal';
 
 export default function Navigation() {
+
+  const [showProfile, setShowProfile] = useState(false)
+
   return (
     <NavBar>
       <NavTitle>WIKINITY</NavTitle>
@@ -13,14 +18,22 @@ export default function Navigation() {
         <NavCategoryLink to='/'>MAIN</NavCategoryLink>
         <NavCategoryLink to='/wiki'>WIKI</NavCategoryLink>
         <NavCategoryLink to='/notice'>NOTICE</NavCategoryLink>
-        <NavCategoryLink to='/project'>PROJECT</NavCategoryLink>
+        <NavCategoryLink to='/projectlist'>PROJECT</NavCategoryLink>
         <NavCategoryLink to='/#'>JOURNAL</NavCategoryLink>
       </NavCategoryBox>
       <NavModalBox>
         <img src={Calendar} alt='calendar' />
         <img src={Todo} alt='todo' />
-        <img src={Profile} alt='profile' />
+        <ProfileBtn onClick={() => setShowProfile(!showProfile)}>
+          <img src={Profile} alt='profile' />
+        </ProfileBtn>
       </NavModalBox>
+      {/* Modals */}
+      {showProfile ?
+        <ProfileModal setShowProfile={setShowProfile} />
+        :
+        null
+      }
     </NavBar>
   );
 }
--- src/recoil/atoms/userState.ts
+++ src/recoil/atoms/userState.ts
@@ -4,11 +4,13 @@ import { atom } from 'recoil';
 interface UserStateDefault {
   isLogin: boolean;
   userInfo: unknown;
+  userData: unknown;
 }
 
 const defaultValue: UserStateDefault = {
   isLogin: false,
   userInfo: null,
+  userData: null,
 };
 
 const { persistAtom } = recoilPersist({
