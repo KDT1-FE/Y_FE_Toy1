a064d19da002fb53c9cda9544013df776f77f39e
Subin Jang <wkdtnqls0506@naver.com>
2023-09-17 23:18:51 +0900

    feat: import firebase storage

--- src/apis/firebase.ts
+++ src/apis/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +17,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore();
+export const storage = getStorage(app);
