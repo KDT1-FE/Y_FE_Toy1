9881bb2d2e5806312216de89b8aaeaa15f853b78
Subin Jang <wkdtnqls0506@naver.com>
2023-09-18 22:47:04 +0900

    refactor: remove unnecessary function

--- src/components/DeleteGallery/index.tsx
+++ src/components/DeleteGallery/index.tsx
@@ -1,15 +1,12 @@
 import { deleteGalleryData } from 'apis/Gallery';
 
 function DeleteGallery(id: string) {
-  const alertConfirm = () => {
-    if (confirm('사진을 삭제하시겠습니까?')) {
-      deleteGalleryData(id);
-      alert('삭제 완료');
-    } else {
-      alert('삭제 취소');
-    }
-  };
-  alertConfirm();
+  if (confirm('사진을 삭제하시겠습니까?')) {
+    deleteGalleryData(id);
+    alert('삭제 완료');
+  } else {
+    alert('삭제 취소');
+  }
 }
 
 export default DeleteGallery;
