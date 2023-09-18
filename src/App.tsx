91e735706a827cb0c674cd654d5283863427f743
NamgungJongMin <jmnamgung@gmail.com>
2023-09-18 14:28:21 +0900

    Style: "Change way to export components"
    export function -> export default function after declared

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { LandingPage } from './views/LandingPage';
-import { LoginPage } from './views/LoginPage';
-import { RegisterPage } from './views/RegisterPage';
+import LandingPage from './views/LandingPage';
+import LoginPage from './views/LoginPage';
+import RegisterPage from './views/RegisterPage';
 import { Company, Notice, Chart, Bylaws } from './views/company';
 import ProjectList from './views/ProjectList';
 
@@ -16,7 +16,7 @@ const App = () => {
             <Route path="/company/chart" element={<Chart />} />
             <Route path="/company/bylaws" element={<Bylaws />} />
           </Route>
-        <Route path="project" element={<ProjectList />} />
+          <Route path="project" element={<ProjectList />} />
         </Route>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
