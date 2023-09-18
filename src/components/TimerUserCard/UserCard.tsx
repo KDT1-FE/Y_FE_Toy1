e4a46c0b67500500407ed0a61ac3bb157932dc46
SOL-MI <ovo10203@gmail.com>
2023-09-18 15:43:30 +0900

    [design] Study 페이지 초기 디자인

--- src/components/TimerUserCard/UserCard.tsx
+++ src/components/TimerUserCard/UserCard.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
 import { userObjects } from 'data/getUser';
 import { Timer } from 'components/TimerUserCard/Timer';
-
-// export interface IAppProps {
-// }
+import './UserCard.scss';
 
 export function UserCard() {
   return (
-    <div>
+    <div className="user-card__container">
       {userObjects.map((user) => (
-        <div key={user.id}>
+        <div className="user-card" key={user.id}>
           <div>
-            {user.nickname}
             <img src={user.image} alt={user.nickname + '님의 사진'}></img>
+            <div className="name">{user.nickname}</div>
           </div>
           <Timer id={user.id} />
         </div>
