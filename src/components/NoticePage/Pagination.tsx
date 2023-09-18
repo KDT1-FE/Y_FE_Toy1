31c934502a7162b15177630a6f6b06ea55da8243
JiHongkyu <hongkyu9941@gmail.com>
2023-09-18 13:04:34 +0900

    Feat: 페이지네이션 이동버튼 구현
    페이지네이션 prev, next버튼 구현, 페이지이동버튼 style 수정

--- src/components/NoticePage/Pagination.tsx
+++ src/components/NoticePage/Pagination.tsx
@@ -10,6 +10,16 @@ function Pagination({ totalPages, setPage }: IPaginationProps) {
   const [currentPage, setCurrentPage] = useState(1);
 
   const handlePageClick = (pageNumber: number) => {
+    if (pageNumber > totalPages) {
+      alert('마지막 페이지입니다.');
+      return;
+    }
+
+    if (pageNumber <= 0) {
+      alert('처음 페이지입니다.');
+      return;
+    }
+
     setCurrentPage(pageNumber);
     setPage(pageNumber);
   };
@@ -23,12 +33,17 @@ function Pagination({ totalPages, setPage }: IPaginationProps) {
     return pageNumbers;
   };
 
-  const pagesToShow = 1; // 한 번에 보여줄 페이지 갯수
+  const pagesToShow = 5; // 한 번에 보여줄 페이지 갯수
   const lastPage = Math.ceil(currentPage / pagesToShow) * pagesToShow;
   const firstPage = lastPage - pagesToShow;
 
   return (
     <S.Wrapper>
+      <S.Page>
+        <S.MoveBtn type='button' onClick={() => handlePageClick(firstPage)}>
+          이전
+        </S.MoveBtn>
+      </S.Page>
       {renderPageNumbers()
         .slice(firstPage, lastPage)
         .map((pageNumber) => (
@@ -39,9 +54,9 @@ function Pagination({ totalPages, setPage }: IPaginationProps) {
           </S.Page>
         ))}
       <S.Page>
-        <S.PageBtn type='button' onClick={() => handlePageClick(lastPage + 1)}>
+        <S.MoveBtn type='button' onClick={() => handlePageClick(lastPage + 1)}>
           다음
-        </S.PageBtn>
+        </S.MoveBtn>
       </S.Page>
     </S.Wrapper>
   );
