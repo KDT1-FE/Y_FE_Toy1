105c77c4f37260555995451523416a4579e07024
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-18 10:53:33 +0900

    Feat: 로그인 유저 ID 전달 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import AppRouter from "./AppRouter";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+
+export type Props = {
+  email?: string;
+  id: string;
+};
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user !== null) {
+        const email = user.email;
+        if (email !== null) {
+          setEmail(email);
+          setId(email.substring(0, email.indexOf("@", 0)));
+        }
+      }
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter />
+      <AppRouter id={id} />
     </>
   );
 }
--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Gallery, Login, Wiki } from "./pages/pageIndex";
 import Header from "./components/common/Header";
+import RootPage from "./components/common/RootPage";
+import { Props } from "./App";
 
-export default function AppRouter() {
+export default function AppRouter({ id }: Props) {
   return (
     <BrowserRouter>
-      <Header />
+      <Header id={id} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/wiki" element={<Wiki />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
+      <RootPage />
     </BrowserRouter>
   );
 }
