88a6fb1c3154dccfef783cfae3729fac7243af3d
Subin Jang <wkdtnqls0506@naver.com>
2023-09-18 22:45:22 +0900

    refactor: asynchronous processing

--- src/apis/Gallery/index.tsx
+++ src/apis/Gallery/index.tsx
@@ -70,9 +70,8 @@ export const getStorage = async () => {
 
 export const deleteGalleryData = async (id: string) => {
   try {
-    await deleteDoc(doc(db, 'gallery', id)).then(() => {
-      window.location.reload();
-    });
+    await deleteDoc(doc(db, 'gallery', id));
+    window.location.reload();
   } catch (error) {
     alert('데이터를 삭제하는 과정에서 오류가 발생했습니다.');
   }
