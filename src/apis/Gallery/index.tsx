e81bda1a9c278f02c35954b150b4b34401715200
Subin Jang <wkdtnqls0506@naver.com>
2023-09-18 19:03:44 +0900

    feat: firebase delete

--- src/apis/Gallery/index.tsx
+++ src/apis/Gallery/index.tsx
@@ -6,8 +6,8 @@ import {
   getDocs,
   query,
   orderBy,
-  // doc,
-  // deleteDoc,
+  doc,
+  deleteDoc,
 } from 'firebase/firestore';
 
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
@@ -65,5 +65,15 @@ export const getStorage = async () => {
     return resultArray;
   } catch (error) {
     alert('오류가 발생했습니다.');
+  }
+};
+
+export const deleteGalleryData = async (id: string) => {
+  try {
+    await deleteDoc(doc(db, 'gallery', id)).then(() => {
+      window.location.reload();
+    });
+  } catch (error) {
+    alert('데이터를 삭제하는 과정에서 오류가 발생했습니다.');
   }
 };
