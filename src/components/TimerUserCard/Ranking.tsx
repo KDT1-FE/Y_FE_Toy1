419e59f01c58abf69a7d780eae4a544fbc21e761
SOL-MI <ovo10203@gmail.com>
2023-09-20 04:41:43 +0900

    [feat] 랭킹 컴포넌트 불러오는 데이터 수정

--- src/components/TimerUserCard/Ranking.tsx
+++ src/components/TimerUserCard/Ranking.tsx
@@ -1,40 +1,52 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
-import {
-  doc,
-  updateDoc,
-  onSnapshot,
-  Unsubscribe,
-  getDoc,
-  orderBy,
-  query,
-  collection,
-} from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { onSnapshot, orderBy, query, collection } from 'firebase/firestore';
 import { db } from 'data/firebase';
+import './Ranking.scss';
+import { formatTime } from 'data/formatTime';
 
 export function Ranking() {
   const docRefs = query(
     collection(db, 'User'),
     orderBy('accumulateCount', 'desc'),
   );
-  const [topThree, setTopThree] = useState([]);
+  const [topThree, setTopThree] = useState<
+    { nickname: string; time: number; image: string }[]
+  >([]);
 
   useEffect(() => {
     onSnapshot(docRefs, (querySnapshot) => {
-      const rankList: any = [];
+      const rankList: { nickname: string; time: number; image: string }[] = [];
+
       querySnapshot.forEach((doc) => {
-        rankList.push(doc.data().nickname);
+        rankList.push({
+          nickname: doc.data().nickname,
+          time: doc.data().accumulateCount,
+          image: doc.data().image,
+        });
       });
       setTopThree(rankList.slice(0, 3));
     });
   }, []);
 
   return (
-    <div>
-      <h1>Top Three Rankings</h1>
-      <ul>
-        {topThree.map((nickname, index) => (
-          <li key={index}>{nickname}</li>
+    <div className={'user-rank__container'}>
+      <h1>👑 랭킹 👑</h1>
+      <ul className={'user-rank__table'}>
+        {topThree.map((entry, index) => (
+          <li
+            className={`user-rank__username${index + 1}`}
+            key={entry.nickname}
+          >
+            <img src={entry.image}></img>
+            <br />
+            {entry.nickname}
+            <br />
+            {formatTime(entry.time)}
+          </li>
         ))}
+        <li></li>
+        <li></li>
+        <li></li>
       </ul>
     </div>
   );
