81149feae28fec8a37f8021aabff41c259ec850f
applevalley <skatkd7571@naver.com>
2023-09-19 15:43:26 +0900

    feat: create wiki update feature

--- src/apis/Wiki/index.tsx
+++ src/apis/Wiki/index.tsx
@@ -1,17 +1,17 @@
-import { getFirestore, collection, addDoc, query, getDocs, where, orderBy, limit, serverTimestamp } from 'firebase/firestore/lite';
+import { getFirestore, collection, doc, setDoc, query, getDocs, where, orderBy, limit, serverTimestamp } from 'firebase/firestore/lite';
 import { app } from 'apis/firebase';
 
 const db = getFirestore(app);
 const wikiRef = collection(db, 'wiki')
-export const create = function(category: string, contents: string) {
+export const create = async function(category: string, contents: string) {
   try {
     console.log('wiki writing success')
-    addDoc(wikiRef, {
+    setDoc(doc(db, `wiki/${category}`), {
       subject: category,
       content: contents,
-      writeTime: serverTimestamp()
-    })
-    return 
+      writeTime: serverTimestamp(),
+      id: wikiRef.id
+    }).then(() => window.location.reload())     
   } catch(e) {
     console.log('wiki writing error')
     console.error(e)
@@ -27,5 +27,20 @@ export const read = async function(category: string) {
     return latestDocument.docs[0].data();
   } else {
     return;
+  }
+}
+
+export const update = async function(category: string, updatedContents: string) {
+  try {
+    const documentForUpdate = doc(wikiRef)
+    await setDoc(doc(db, `wiki/${category}`), {
+      subject: category,
+      content: updatedContents,
+      id: documentForUpdate.id,
+      writeTime: serverTimestamp()
+    }).then(() => window.location.reload())
+  } catch(e) {
+    console.log('wiki update failed')
+    console.error(e)
   }
 }
--- src/pages/Wiki/index.tsx
+++ src/pages/Wiki/index.tsx
@@ -2,21 +2,25 @@ import NavigationWiki from 'components/NavigationWiki';
 import styled from 'styled-components';
 import MDEditor from '@uiw/react-md-editor';
 import { useState, useEffect } from 'react'
-import { create, read } from 'apis/Wiki';
+import { create, read, update } from 'apis/Wiki';
 import { useLocation } from 'react-router-dom'
 import { Timestamp } from 'firebase/firestore'
 
-interface props {
+interface createProps {
   setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function WikiCreate({ setIsEdit }: props) {
+interface updateProps {
+  setIsUpdate: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function WikiCreate({ setIsEdit }: createProps) {
   const location = useLocation()
   const searchParams = new URLSearchParams(location.search)
   let selectedCategory = searchParams.get('category')
   if (selectedCategory === null) selectedCategory = 'companyRule'
   const [textValue, setTextValue] = useState('')  
-  const handleSetValue = (text: string | undefined) => {
+  const handleSetValue = (text: string) => {
     if (text) {
       setTextValue(text);
     }
@@ -37,42 +41,83 @@ function WikiCreate({ setIsEdit }: props) {
       <MDEditor
         placeholder='등록할 내용을 입력해주세요.'
         value={textValue}
-        onChange={(event) => {handleSetValue(event as string | undefined)}}
+        onChange={(event) => {handleSetValue(event as string)}}
         id='markdownEditor'
       />
     </TextareaContainer>
-  )
-  
+  )  
 }
 
+
+function WikiUpdate({ setIsUpdate }: updateProps) {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  let selectedCategory = searchParams.get('category');
+  if (selectedCategory === null) selectedCategory = 'companyRule';
+  const [textValue, setTextValue] = useState('');
+  const handleSetValue = (text: string) => {
+    if (text) {
+      setTextValue(text);
+    }
+  };
+
+  return (
+    <TextareaContainer>
+      <ButtonContainer>
+        <button
+          onClick={() => {
+            if (textValue === '') {
+              alert('빈 내용은 등록하실 수 없습니다.');
+              return;
+            }
+            update(selectedCategory as string, textValue);
+            setIsUpdate(false);
+          }}
+        >
+          등록하기
+        </button>
+      </ButtonContainer>
+      <MDEditor
+        placeholder="등록할 내용을 입력해주세요."
+        value={textValue}
+        onChange={(event) => {
+          handleSetValue(event as string);
+        }}
+        id="markdownEditor"
+      />
+    </TextareaContainer>
+  );
+}
+
+
 function Wiki() {
-  const [isEdit, setIsEdit] = useState(false)
-  const [isDocumentExist, setDocumentExist] = useState(false)
+  const [isEdit, setIsEdit] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [isDocumentExist, setDocumentExist] = useState(false);
   const [data, setData] = useState(Object);
-  const [documentTime, setDocumentTime] = useState('')
-  const location = useLocation()  
-  const searchParams = new URLSearchParams(location.search)
-  let selectedCategory = searchParams.get('category')
-  if (selectedCategory === null) selectedCategory = 'companyRule'
+  const [documentTime, setDocumentTime] = useState('');
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  let selectedCategory = searchParams.get('category');
+  if (selectedCategory === null) selectedCategory = 'companyRule';
 
   const getDocumentList = async () => {
-    const document = await read(selectedCategory as string); 
-    document === undefined ? setDocumentExist(false) : setDocumentExist(true)
-    setData(document)
-    const time = document?.writeTime as Timestamp
+    const document = await read(selectedCategory as string);
+    document === undefined ? setDocumentExist(false) : setDocumentExist(true);
+    setData(document);
+    const time = document?.writeTime as Timestamp;
     if (time) {
-      setDocumentTime(time.toDate().toString())
-    } 
-  }
+      setDocumentTime(time.toDate().toString());
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       const documentData = await getDocumentList();
-      return documentData
-    }
+      return documentData;
+    };
     fetchData();
   }, [selectedCategory]);
-
 
   return (
     <WikiContainer>
@@ -82,31 +127,51 @@ function Wiki() {
           <WikiCreate setIsEdit={setIsEdit}></WikiCreate>
         ) : (
           <TextareaContainer>
-            { isDocumentExist === false ? 
-              <div onClick={() => {
-                setIsEdit(true)
-              }}>
-                <h1>아직 작성된 글이 없습니다.</h1>
-                글 작성하기
-              </div> : 
-                <div className='document'>
-                  <ButtonContainer>
-                    <div>
-                      <button>수정하기</button>
-                      <button>삭제하기</button>
-                    </div>
-                  </ButtonContainer>
-                  <p>최종 수정 시간: {documentTime}</p>
-                  <br/>
-                  <MDEditor.Markdown className='markdownViewer' source={data?.content} /> 
-                </div>
-            }
+            {isDocumentExist === false ? (
+              <div
+                onClick={() => {
+                  setIsEdit(true);
+                }}
+              >
+                <h1>아직 작성된 글이 없습니다.</h1>글 작성하기
+              </div>
+            ) : (
+              <div className="document">
+                {isUpdate === true ? (
+                  <WikiUpdate
+                    setIsUpdate={setIsUpdate}
+                  ></WikiUpdate>
+                ) : (
+                  <div>
+                    <ButtonContainer>
+                      <div>
+                        <button
+                          onClick={() => {
+                            setIsUpdate(true);
+                          }}
+                        >
+                          수정하기
+                        </button>
+                        <button>삭제하기</button>
+                      </div>
+                    </ButtonContainer>
+                    <p>최종 수정 시간: {documentTime}</p>
+                    <br />
+                    <MDEditor.Markdown
+                      className="markdownViewer"
+                      source={data?.content}
+                    />
+                  </div>
+                )}
+              </div>
+            )}
           </TextareaContainer>
         )}
       </div>
     </WikiContainer>
-  )
+  );
 }
+
 
 const WikiContainer = styled.div`
   display: grid;
