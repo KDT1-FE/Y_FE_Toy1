013aadc73689fc5a716c74737b2c8a508e942859
turkey-kim <rlaxmrgml@naver.com>
2023-09-19 17:09:16 +0900

    refact: assets/images 생성 후 파일 이동

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -1,15 +1,15 @@
 import React, {useState, useEffect} from "react";
-import "../../styles/Carousel.css";
-import img1 from "../../assets/slide01.jpg";
-import img2 from "../../assets/slide02.jpg";
-import img3 from "../../assets/slide03.jpg";
-import img4 from "../../assets/slide04.jpg";
-import img5 from "../../assets/slide05.jpg";
-import img6 from "../../assets/slide06.jpg";
-import img7 from "../../assets/slide07.jpg";
-import img8 from "../../assets/slide08.jpg";
-import img9 from "../../assets/slide09.jpg";
-import img10 from "../../assets/slide10.jpg";
+import "../../styles/home/carousel.css";
+import img1 from "../../assets/images/slide01.jpg";
+import img2 from "../../assets/images/slide02.jpg";
+import img3 from "../../assets/images/slide03.jpg";
+import img4 from "../../assets/images/slide04.jpg";
+import img5 from "../../assets/images/slide05.jpg";
+import img6 from "../../assets/images/slide06.jpg";
+import img7 from "../../assets/images/slide07.jpg";
+import img8 from "../../assets/images/slide08.jpg";
+import img9 from "../../assets/images/slide09.jpg";
+import img10 from "../../assets/images/slide10.jpg";
 
 function Carousel() {
   const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
