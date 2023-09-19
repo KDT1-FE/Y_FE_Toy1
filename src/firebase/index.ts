97ec244392988cb603f53a8b5c38a2162bfe1ebc
developer-jyyun <developer.jyyun@gmail.com>
2023-09-19 19:47:25 +0900

    Feat: "Modify image upload path"

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,6 +6,6 @@ import { getStorage, ref } from 'firebase/storage';
 const db = getFirestore(app);
 const storage = getStorage(app);
 const galleryCollection = collection(db, 'gallery');
-const galleryStorageRef = ref(storage, 'gallery/');
+const galleryStorageRef = ref(storage, 'images');
 
 export { db, storage, galleryCollection, galleryStorageRef };
