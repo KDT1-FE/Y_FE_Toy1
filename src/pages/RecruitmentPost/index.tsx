6d0709150474a574aa1b0fca6e0e408e3175c0eb
JinJeongMin <jdsjm2@naver.com>
2023-09-19 15:50:14 +0900

    Feat: 모집 인원 min,max 구현

--- src/pages/RecruitmentPost/index.tsx
+++ src/pages/RecruitmentPost/index.tsx
@@ -24,6 +24,10 @@ const RecruitmentPost: React.FC = () => {
     const [userId, setUserId] = useRecoilState(UserId);
     const [categoryToggle, setCategoryToggle] = useState(true);
     const [userName, setUserName] = useState('');
+    const [peopleValue, setPeopleValue] = useState(1);
+
+    const min = 1;
+    const max = 50;
 
     const navigate = useNavigate();
 
@@ -125,14 +129,24 @@ const RecruitmentPost: React.FC = () => {
                         )}
                     </PostBox>
                     <PostBox>
-                        <PostH>모집 인원</PostH>
+                        <PostH>모집 인원(최대 50명)</PostH>
                         <TextField
                             id="standard-basic"
                             label="모집 인원"
                             variant="standard"
                             type="number"
                             name="people"
-                            placeholder="00명"
+                            placeholder="1"
+                            inputProps={{ min, max }}
+                            value={peopleValue}
+                            onChange={(e) => {
+                                let peopleValue = parseInt(e.target.value, 10);
+
+                                if (peopleValue > max) peopleValue = max;
+                                if (peopleValue < min) peopleValue = min;
+
+                                setPeopleValue(peopleValue);
+                            }}
                             style={{ width: '150px' }}
                         />
                     </PostBox>
