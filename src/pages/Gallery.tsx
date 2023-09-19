7b88efd914fd56945729defac393ea294c739044
furaha707 <zxcvyein950823@gmail.com>
2023-09-19 14:29:01 +0900

    Feat: 사이드바 css

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -7,72 +7,41 @@ import { Routes, Route } from "react-router-dom"
 import styled from 'styled-components'
 import { db } from "../firebase"
 import { collection, getDocs, query, where } from  "firebase/firestore"
-
-interface userData {
-  id?: string,
-  category?: string,
-  title?: string,
-  date?: string,
-  timestamp?: string,
-  writer?: string,
-  desc?: string,
-  thumbnail?: string,
-  uid?: string
-}
-
+import userData from './UserData'
 
 const Gallery = () => {
   
   const [onEdit, setOnEdit] = useState<boolean>(false);
   const [galleryData, setGalleryData] = useState<userData[]>([]);
-
-  // 사이드바 쿼리구현
-    const handleClick = async (category: string) => {
-      const q = query(collection(db, "gallery"), where("category", "==", category));
-      const data = await getDocs(q);
-      setGalleryData(data.docs.map((doc) => ({
-        id: doc.id,
-        category: doc.data().category,
-        title: doc.data().title,
-        date: doc.data().date,
-        timestamp: doc.data().timestamp,
-        writer: doc.data().writer,
-        desc: doc.data().desc,
-        thumbnail: doc.data().thumbnail,
-        uid: doc.data().uid
-      })));
+  const [activeCategory, setActiveCategory] = useState('notice'); // 초기값 설정
+  // 초기값
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDocs(collection(db, "gallery"));
+      const galleryData = data.docs.map((doc) => ({...doc.data(), id: doc.id}));
+      setGalleryData(galleryData);
     };
 
-    // 초기값
-    useEffect(() => {
-      const fetchData = async () => {
-        const data = await getDocs(collection(db, "gallery"));
-        const galleryData = data.docs.map((doc) => ({
-          id: doc.id,
-          category: doc.data().category,
-          title: doc.data().title,
-          date: doc.data().date,
-          timestamp: doc.data().timestamp,
-          writer: doc.data().writer,
-          desc: doc.data().desc,
-          thumbnail: doc.data().thumbnail,
-          uid: doc.data().uid
-        }));
-        setGalleryData(galleryData);
-      };
-  
-      fetchData();
-    }, []);
+    fetchData();
+  }, []);
+
+  // 사이드바 쿼리구현
+  const handleClick = async (category: string) => {
+    const q = query(collection(db, "gallery"), where("category", "==", category));
+    const data = await getDocs(q);
+    setGalleryData(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    setActiveCategory(category)
+  };
 
   return (
   <>
-    <GallerySidebar handleClick={handleClick}/>
+    <GallerySidebar handleClick={handleClick} activeCategory={activeCategory}/>
     <Container>
       <Routes>
         <Route path="" element={<GalleryList galleryData={galleryData} />} />
-        <Route path="edit" element={<GalleryEdit onEdit={onEdit} setOnEdit={setOnEdit}/>} />
-        <Route path="edit/:id" element={<GalleryEdit onEdit={onEdit} setOnEdit={setOnEdit} />} />
-        <Route path="detail/:id" element={<GalleryDetail onEdit={onEdit} setOnEdit={setOnEdit} />} />
+        <Route path="edit" element={<GalleryEdit onEdit={onEdit} setOnEdit={setOnEdit} setGalleryData={setGalleryData} />} />
+        <Route path="edit/:id" element={<GalleryEdit onEdit={onEdit} setOnEdit={setOnEdit} setGalleryData={setGalleryData} />} />
+        <Route path="detail/:id" element={<GalleryDetail onEdit={onEdit} setOnEdit={setOnEdit} setGalleryData={setGalleryData} />} />
       </Routes>
     </Container>
   </>
