c4db4d2c9a2ee35627ea2b525b254956c1bd8eaa
JitHoon <imessi0336@gmail.com>
2023-09-20 18:38:40 +0900

    refact: 사용자가 데이터 변경 시 sessionStorage에도 업데이트

--- src/components/Wiki/PostTeam.tsx
+++ src/components/Wiki/PostTeam.tsx
@@ -1,10 +1,26 @@
 import {updateDoc, doc} from "firebase/firestore";
 import {db} from "../../utils/firebaseConfig";
 
-async function PostTeam(dataKey: string, newContent: string): Promise<void> {
+async function PostTeam(
+  dataKey: string,
+  newContent: string,
+  멘티: string,
+  멘토: string,
+  img: string,
+): Promise<void> {
   await updateDoc(doc(db, "team", dataKey), {
     content: newContent,
   });
+
+  sessionStorage.setItem(
+    dataKey,
+    JSON.stringify({
+      멘티,
+      멘토,
+      img,
+      content: newContent,
+    }),
+  );
 }
 
 export default PostTeam;
