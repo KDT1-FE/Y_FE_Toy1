06577c9a46edff6c46bec3437adde8b47dabb118
kyu <qorudrb14@naver.com>
2023-09-20 14:59:27 +0900

    [fix]권한 없으면 글쓰기나 글수정,삭제  이동시 404페이지 이동

--- src/components/Wiki/BoardContent.tsx
+++ src/components/Wiki/BoardContent.tsx
@@ -46,7 +46,7 @@ export function BoardContent({ boardState }: any) {
     data.then((item: any) => {
       setboardData(item);
     });
-  }, [boardState]);
+  }, [boardState,sessionStorage.uid]);
 
   return (
     <div className="board__content">
