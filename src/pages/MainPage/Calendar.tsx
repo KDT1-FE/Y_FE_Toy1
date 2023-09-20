9c47da8f4af449f73326412abdac7df84b8ac8dd
jinjoo-jung <wjdwlswn23@naver.com>
2023-09-20 12:37:32 +0900

    Feat: Calendar 축가

---
+++ src/pages/MainPage/Calendar.tsx
@@ -0,0 +1,40 @@
+import moment from "moment";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styled from "styled-components";
+
+const CustomCalendarContainer = styled.div`
+  /* 원하는 스타일을 여기에 추가하세요 */
+  max-width: 1200px; /* 예시: 컬럼의 너비를 늘릴 때 */
+  margin: 300px auto;
+  display: flex;
+  flex-direction: column;
+  padding: 0 100px;
+
+  .react-calendar {
+    line-height: 5em;
+  }
+`;
+
+const CustomCalendar = styled(Calendar)`
+  /* Calendar 컴포넌트에 대한 추가 스타일을 여기에 추가하세요 */
+  /* 예시: 크기 조정 */
+  width: 100%;
+  background-color: rgba(150, 160, 255, 0.2);
+  height: 400px; /* 원하는 높이로 조절 */
+`;
+
+function Calendars() {
+  const [value, onChange] = useState(new Date());
+  return (
+    <CustomCalendarContainer>
+      <div className="text-gray-500 mt-4">
+        {moment(value).format("YYYY년 MM월 DD일")}
+      </div>
+      <CustomCalendar onClickDay={onChange} value={value} />
+    </CustomCalendarContainer>
+  );
+}
+
+export default Calendars;
--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+// import Calendar from "@fullcalendar/react";
+// import dayGridPlugin from "@fullcalendar/daygrid"; // dayGrid 플러그인 추가
+import Calendars from "./Calendar";
 
 export default function Main() {
-
   return (
     <div>
-      Main
+      <Calendars />
     </div>
-  )
+  );
 }
