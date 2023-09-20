ae820716eea07c1fed517d0da2232a3faebcec19
Taegwan Youn <tkyoun0421@naver.com>
2023-09-20 10:35:06 +0900

    Feat: Remove toastUi

--- src/firebase/fireStore.ts
+++ src/firebase/fireStore.ts
@@ -1,1 +1,6 @@
 import app from './config';
+import { getFirestore } from 'firebase/firestore';
+
+const db = getFirestore(app);
+
+export default db;
--- src/views/NoticeWritePage.tsx
+++ src/views/NoticeWritePage.tsx
@@ -1,36 +1,57 @@
-import React, { useState } from 'react';
-import { Editor } from '@toast-ui/react-editor';
-import { ChangeEvent } from 'react';
+import React, { EventHandler, useState } from 'react';
 
 import '../scss/components/writePage/writePage.scss';
-import '@toast-ui/editor/dist/toastui-editor.css';
 
 const NoticeWritePage = () => {
-  const [contentTitle, setContentTitle] = useState<string>('');
-  // const [contentText, setContentText] = useState<string>('');
-  const handleContentTitle = (e: ChangeEvent) => {
-    const target = e.target as HTMLInputElement;
-    setContentTitle(target.value);
+  const [notice, setNotice] = useState({});
+  const [file, setFile] = useState<File | null>();
+  const [content, setContent] = useState<string>();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      setFile(files && files[0]);
+      return;
+    }
+    setNotice(item => ({ ...item, [name]: value }));
+  };
+
+  const handleContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
   };
 
   return (
     <form className="write-form" onSubmit={handleSubmit}>
       <div className="write-form__title">
-        <textarea
+        <input
+          type="text"
+          name="title"
           className="write-form__title__item"
           placeholder="제목을 입력하세요"
-          onChange={handleContentTitle}
-          value={contentTitle}></textarea>
+          onChange={handleChange}
+          required></input>
         ;
       </div>
-      <Editor initialEditType="markdown" height="400px" initialValue=" "></Editor>
-      <button type="submit" className="write-form__button btn">
-        완료
-      </button>
+      <div className="write-form__content">
+        <textarea name="content" className="write-form__content__item" onChange={handleContent}></textarea>
+      </div>
+      <div className="write-util">
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          required
+          className="write-util__image"
+          onChange={handleChange}
+        />
+        <button type="submit" className="write-form__button btn">
+          완료
+        </button>
+      </div>
     </form>
   );
 };
