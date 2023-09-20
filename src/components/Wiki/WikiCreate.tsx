abf4e83c52f11872a945ce40432994ab444c03b4
applevalley <skatkd7571@naver.com>
2023-09-20 18:12:02 +0900

    refactor: seperate create/update component

---
+++ src/components/Wiki/WikiCreate.tsx
@@ -0,0 +1,108 @@
+import MDEditor from '@uiw/react-md-editor';
+import { create, update } from 'apis/Wiki';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { media } from 'styles/media';
+
+interface createProps {
+  setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedCategory: string;
+  data: any;
+}
+
+function WikiCreate({ setIsEdit, selectedCategory, data }: createProps) {
+  const isCreate = data === undefined;
+  const [textValue, setTextValue] = useState(isCreate ? '' : data.content);
+  const handleSetValue = (text: string) => {
+    if (text) {
+      setTextValue(text);
+    }
+  };
+
+  const handleWikiContent = async () => {
+    if (textValue === '') {
+      alert('빈 내용은 등록하실 수 없습니다.');
+      return;
+    }
+    if (isCreate) {
+      await create(selectedCategory, textValue);
+      setIsEdit(false);
+    } else {
+      await update(selectedCategory, textValue);
+      setIsEdit(false);
+    }
+  };
+
+  return (
+    <StyledTextareaContainer>
+      <ButtonContainer>
+        <button onClick={handleWikiContent}>
+          {isCreate ? '등록하기' : '수정하기'}
+        </button>
+      </ButtonContainer>
+      <MDEditor
+        placeholder="등록할 내용을 입력해주세요."
+        value={textValue}
+        onChange={(event) => {
+          handleSetValue(event as string);
+        }}
+        id="markdownEditor"
+      />
+    </StyledTextareaContainer>
+  );
+}
+
+
+export default WikiCreate;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    margin: 1rem;
+  }
+`;
+
+const StyledTextareaContainer = styled.div`
+  margin: 2rem;
+  position: relative;
+
+  #markdownEditor {
+    height: 100% !important;
+  }
+
+  .document {
+    height: 100%;
+  }
+
+  .document .markdownViewer {
+    height: 100%;
+    overflow-y: scroll;
+  }
+
+  .document .markdownViewer::-webkit-scrollbar {
+    display: none;
+  }
+
+  ${media.desktop_lg(`
+    width: 35rem;
+    height: 40rem; 
+  `)}
+  ${media.tablet(`
+    width: 30rem;
+    height: 35rem;
+  `)}
+  ${media.tablet_680(`
+    width: 25rem;
+    height: 35rem;
+  `)}
+  ${media.tablet_625(`
+    width: 20rem;
+    height: 30rem;
+  `)}
+  ${media.mobile_430(`
+    width: 15rem;
+    height: 25rem;
+  `)}
+`;
--- src/pages/Wiki/index.tsx
+++ src/pages/Wiki/index.tsx
@@ -1,110 +1,19 @@
 import NavigationWiki from 'components/NavigationWiki';
 import styled from 'styled-components';
-import MDEditor from '@uiw/react-md-editor';
 import { useState, useEffect } from 'react';
-import { create, read } from 'apis/Wiki';
+import {  read } from 'apis/Wiki';
 import { useLocation } from 'react-router-dom';
-// import { Timestamp } from 'firebase/firestore';
 import { media } from 'styles/media';
 import Loading from 'components/Common/Loading';
 import WikiContent from 'components/Wiki/'
-
-
-interface createProps {
-  setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-// interface updateProps {
-//   setIsUpdate: React.Dispatch<React.SetStateAction<boolean>>;
-// }
-
-function WikiCreate({ setIsEdit }: createProps) {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  let selectedCategory = searchParams.get('category');
-  if (selectedCategory === null) selectedCategory = 'companyRule';
-  const [textValue, setTextValue] = useState('');
-  const handleSetValue = (text: string) => {
-    if (text) {
-      setTextValue(text);
-    }
-  };
-
-  return (
-    <StyledTextareaContainer>
-      <ButtonContainer>
-        <button
-          onClick={() => {
-            if (textValue === '') {
-              alert('빈 내용은 등록하실 수 없습니다.');
-              return;
-            }
-            create(selectedCategory as string, textValue);
-            setIsEdit(false);
-          }}
-        >
-          등록하기
-        </button>
-      </ButtonContainer>
-      <MDEditor
-        placeholder="등록할 내용을 입력해주세요."
-        value={textValue}
-        onChange={(event) => {
-          handleSetValue(event as string);
-        }}
-        id="markdownEditor"
-      />
-    </StyledTextareaContainer>
-  );
-}
-
-// function WikiUpdate({ setIsUpdate }: updateProps) {
-//   const location = useLocation();
-//   const searchParams = new URLSearchParams(location.search);
-//   let selectedCategory = searchParams.get('category');
-//   if (selectedCategory === null) selectedCategory = 'companyRule';
-//   const [textValue, setTextValue] = useState('');
-//   const handleSetValue = (text: string) => {
-//     if (text) {
-//       setTextValue(text);
-//     }
-//   };
-
-//   return (
-//     <StyledTextareaContainer>
-//       <ButtonContainer>
-//         <button
-//           onClick={() => {
-//             if (textValue === '') {
-//               alert('빈 내용은 등록하실 수 없습니다.');
-//               return;
-//             }
-//             update(selectedCategory as string, textValue);
-//             setIsUpdate(false);
-//           }}
-//         >
-//           등록하기
-//         </button>
-//       </ButtonContainer>
-//       <MDEditor
-//         placeholder="등록할 내용을 입력해주세요."
-//         value={textValue}
-//         onChange={(event) => {
-//           handleSetValue(event as string);
-//         }}
-//         id="markdownEditor"
-//       />
-//     </StyledTextareaContainer>
-//   );
-// }
+import WikiCreate from 'components/Wiki'
 
 function Wiki() {
   const [isEdit, setIsEdit] = useState(false);
-  // const [isUpdate, setIsUpdate] = useState(false);
-  // const [isDocumentExist, setDocumentExist] = useState(false);
   const [data, setData] = useState(Object);
-  // const [documentTime, setDocumentTime] = useState('');
+  // const [isChange, setIsChanged] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   let selectedCategory = searchParams.get('category');
@@ -112,86 +21,44 @@ function Wiki() {
 
   const getDocumentList = async () => {
     setIsLoading(true)
-    const document = await read(selectedCategory as string);
-    // document === undefined ? setDocumentExist(false) : setDocumentExist(true);
+    if (selectedCategory === null) return;
+    const document = await read(selectedCategory);
     setData(document);
-    // const time = document?.writeTime as Timestamp;
-    // if (time) {
-    //   setDocumentTime(time.toDate().toString());
-    // }
     setIsLoading(false)
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const documentData = await getDocumentList();
-      return documentData;
-    };
-    fetchData();
-  }, [selectedCategory]);
+    getDocumentList();
+  }, [isEdit]);
+
+  // useEffect(() => {
+  //   if (isEdit) setIsEdit(false);
+  //   getDocumentList();
+  // }, [isChange])
 
   return (
     <StyledWikiContainer>
-      <NavigationWiki></NavigationWiki>
-
-      {isEdit === true ? (
-        <WikiCreate setIsEdit={setIsEdit}></WikiCreate>
-      ) : (
-        <StyledTextareaContainer>
-          {isLoading ? (
-            <Loading></Loading>
-          ) : (
-            <WikiContent data={data} setIsEdit={setIsEdit} />
-          )}
-          {
-            <></> /* {isDocumentExist === false ? (
-            <StyledWikiNotExist>
-              <h1>아직 등록된 글이 없습니다.</h1>
-              <button
-                onClick={() => {
-                  setIsEdit(true);
-                }}
-              >
-                + 글 작성하기
-              </button>
-            </StyledWikiNotExist>
-          ) : (
-            <div className="document">
-              {isUpdate === true ? (
-                <WikiUpdate setIsUpdate={setIsUpdate}></WikiUpdate>
-              ) : (
-                <div>
-                  <ButtonContainer>
-                    <div>
-                      <button
-                        onClick={() => {
-                          setIsUpdate(true);
-                        }}
-                      >
-                        수정하기
-                      </button>
-                      <button
-                        onClick={() => {
-                          wikiDelete(selectedCategory as string);
-                        }}
-                      >
-                        삭제하기
-                      </button>
-                    </div>
-                  </ButtonContainer>
-                  <p>최종 수정 시간: {documentTime}</p>
-                  <br />
-                  <MDEditor.Markdown
-                    className="markdownViewer"
-                    source={data?.content}
-                  />
-                </div>
-              )}
-            </div>
-          )} */
-          }
-        </StyledTextareaContainer>
-      )}
+      <NavigationWiki
+        // setIsChanged={setIsChanged}
+        // isChange={isChange}
+      ></NavigationWiki>
+      <div>
+        {isEdit ? (
+          <WikiCreate
+            setIsEdit={setIsEdit}
+            data={data}
+            // selectedCategory={selectedCategory}
+          ></WikiCreate>
+        ) : (
+          <StyledTextareaContainer>
+            {isLoading ? (
+              <Loading></Loading>
+            ) : (
+              <WikiContent data={data} setIsEdit={setIsEdit} />
+            )}
+          </StyledTextareaContainer>
+        )}
+      </div>
     </StyledWikiContainer>
   );
 }
@@ -202,14 +69,6 @@ const StyledWikiContainer = styled.div`
   grid-template-columns: 0.2fr 0.8fr;
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-
-  button {
-    margin: 1rem;
-  }
-`;
 
 const StyledTextareaContainer = styled.div`
   margin: 2rem;
