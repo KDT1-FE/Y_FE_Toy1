b98b20c989eb5bc22feb2b785ac65e581dbf4e43
JitHoon <imessi0336@gmail.com>
2023-09-20 18:31:23 +0900

    refact: 쉬운 데이터 읽기를 위한 url key에 공백 추가

--- src/components/Wiki/WikiSidebar.tsx
+++ src/components/Wiki/WikiSidebar.tsx
@@ -20,17 +20,17 @@ function WikiSidebar() {
         >
           커리큘럼
         </Link>
-        <Link to="/wiki/교육생소개">교육생 소개</Link>
-        <Link to="/wiki/팀구성">팀 구성</Link>
+        <Link to="/wiki/교육생 소개">교육생 소개</Link>
+        <Link to="/wiki/팀 구성">팀 구성</Link>
       </div>
       <span className="WikiCategory">프로젝트</span>
       <div className="CategoryListWrapper">
-        <Link to="/wiki/진행중인프로젝트">진행 중인 프로젝트</Link>
-        <Link to="/wiki/완료된프로젝트">완료된 프로젝트</Link>
+        <Link to="/wiki/진행 중인 프로젝트">진행 중인 프로젝트</Link>
+        <Link to="/wiki/완료된 프로젝트">완료된 프로젝트</Link>
       </div>
       <span className="WikiCategory">정책</span>
       <div className="CategoryListWrapper">
-        <Link to="/wiki/휴가정책">휴가 정책</Link>
+        <Link to="/wiki/휴가 정책">휴가 정책</Link>
         <Link to="/wiki/복리후생">복리후생</Link>
       </div>
     </div>
