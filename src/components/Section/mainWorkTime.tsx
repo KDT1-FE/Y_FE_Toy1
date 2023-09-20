927231eeb002c6fe6c9c325b3b718f8767209402
zoeyourlife <201814112@office.skhu.ac.kr>
2023-09-20 14:41:47 +0900

    feat: main Component Worktime CONST
    상수화 관리 이후 메인 작업시간 컴포넌트 분리

---
+++ src/components/Section/mainWorkTime.tsx
@@ -0,0 +1,49 @@
+import {
+  MAIN_WORKTIME_TITLE,
+  MAIN_WORKTIME_SUBTITLE,
+} from 'constants/maintext';
+import styled from 'styled-components';
+import { media } from 'styles/media';
+
+function MainWorkTime() {
+  return (
+    <>
+      <StyledTitleTextBox>
+        <StyledWorkTimeText>{MAIN_WORKTIME_TITLE}</StyledWorkTimeText>
+        <StyledWorkTimeSmallText>
+          {MAIN_WORKTIME_SUBTITLE}
+        </StyledWorkTimeSmallText>
+      </StyledTitleTextBox>
+    </>
+  );
+}
+
+const StyledTitleTextBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+const StyledWorkTimeText = styled.span`
+  font-size: 2rem;
+  font-weight: 500;
+  margin-left: 1rem;
+
+  ${media.tablet(`
+  display: none;
+`)}
+`;
+
+const StyledWorkTimeSmallText = styled.span`
+  font-size: 0.8rem;
+  font-weight: 400;
+  margin-left: 1rem;
+  margin-bottom: 0.5rem;
+
+  ${media.tablet(`
+  font-size: .8rem
+`)}
+`;
+
+export default MainWorkTime;
