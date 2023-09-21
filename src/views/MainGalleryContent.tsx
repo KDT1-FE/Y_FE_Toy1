d3a19a6bca07bac41cb8071cdcebc3e0a85e8647
developer-jyyun <developer.jyyun@gmail.com>
2023-09-21 20:18:23 +0900

    Feat: "Add main gallery content"

---
+++ src/views/MainGalleryContent.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { UploadedImage } from '../components/Gallery/types';
+import useMainGalleryList from '../hooks/gallery/query/useMainGalleryList';
+
+import '../scss/mainGallery.scss';
+
+const MainGalleryContents = () => {
+  // const queryClient = useQueryClient();
+  const { data: mainGalleryList } = useMainGalleryList();
+  return (
+    <Link to="/gallery">
+      <div className="mainPage__content__gallery">
+        {mainGalleryList.map((item: UploadedImage) => {
+          return (
+            <figure key={item.id} style={{ backgroundImage: `url(${item.url})` }} onClick={() => {}}>
+              <figcaption>
+                <h3>{item.title}</h3>
+                <p>{item.timestamp}</p>
+              </figcaption>
+            </figure>
+          );
+        })}
+      </div>
+    </Link>
+  );
+};
+
+export default MainGalleryContents;
