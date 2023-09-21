a35fafca05e21b3a36a313ab3eab9e1da99f1d0a
SOL-MI <ovo10203@gmail.com>
2023-09-21 13:59:43 +0900

    [feat] 날짜 변경 시 초기화

--- src/components/TimerUserCard/Timer.tsx
+++ src/components/TimerUserCard/Timer.tsx
@@ -31,6 +31,9 @@ export function Timer({ id }: TimerProps): JSX.Element {
   const [count, setCount] = useState(0);
   const intervalRef = useRef<number | null>(null); // interval ID 저장
   const [isTimerRunning, setIsTimerRunning] = useState(false);
+  // const [currentDate, setCurrentDate] = useState(
+  //   getCurrentDateFromStamp(timeStamp),
+  // );
 
   let unsubscribe: Unsubscribe | undefined;
   let timer: any;
@@ -75,7 +78,6 @@ export function Timer({ id }: TimerProps): JSX.Element {
       if (!isTimerRunning) {
         // 타이머 시작 (1초마다 호출)
         timer = setInterval(async () => {
-          console.log('3');
           const docSnapshot = await getDoc(chosenUserRef);
           if (docSnapshot.exists()) {
             const userData = docSnapshot.data();
@@ -93,7 +95,6 @@ export function Timer({ id }: TimerProps): JSX.Element {
       //실시간 업데이트 수신 중지
       unsubscribe = onSnapshot(chosenUserRef, async (docSnapshot) => {
         if (docSnapshot.exists()) {
-          console.log('4');
           const userData = docSnapshot.data();
           setCount(userData.accumulateCount);
         } else {
@@ -120,17 +121,32 @@ export function Timer({ id }: TimerProps): JSX.Element {
     setIsTimerRunning(false);
   }, []);
 
-  //시간 초기화
-  const reset: () => void = useCallback(() => {
-    setCount(0);
-  }, []);
+  // 날짜 변경 감지 및 count 초기화
+  useEffect(() => {
+    const userRef = doc(db, 'User', id);
+    // 간격을 두고 실행
+    const checkDateInterval = setInterval(async () => {
+      const docSnapshot = await getDoc(userRef);
+      if (docSnapshot.exists()) {
+        const userData = docSnapshot.data();
+        const serverTimestamp = userData.serverTimestamp; // Firestore의 서버 타임스탬프
+        const currentServerTime = Math.floor(new Date().getTime() / 1000);
+
+        // 서버 시간과 Firestore의 서버 타임스탬프를 비교하여 날짜 변경 확인
+        if (currentServerTime > serverTimestamp) {
+          setCount(0);
+          // Firestore에 count 업데이트
+          await updateDoc(userRef, { accumulateCount: 0 });
+        }
+      }
+    }, 1800000); // 30분마다 체크
+
+    return () => {
+      clearInterval(checkDateInterval);
+    };
+  }, [id]);
 
   const formattedTime = formatTime(count);
-
-  const currentDate = getCurrentDateFromStamp(timeStamp);
-  useEffect(() => {
-    reset();
-  }, [currentDate]);
 
   return (
     <div>
