7c04c15d00817527b7ac5d05a1fb23b11a28d47f
applevalley <skatkd7571@naver.com>
2023-09-21 18:50:15 +0900

    design: add responsive navigation bar

--- src/components/NavigationWiki/index.tsx
+++ src/components/NavigationWiki/index.tsx
@@ -3,6 +3,7 @@ import { Container } from 'components/NavigationContainer';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
 import { CATEGORY } from 'constants/wiki';
+import { media } from 'styles/media';
 
 interface INavigationWikiProps {
   setIsChanged: React.Dispatch<React.SetStateAction<boolean>>;
@@ -24,7 +25,7 @@ function NavigationWiki({ setIsChanged, isChange }: INavigationWikiProps) {
   const selectedCategory = searchParams.get('category');
 
   return (
-    <Container>
+    <StyledWikiContainer>
       <StyledCategoryContainer>
         {CATEGORY.map((category: ICategory, categoryIndex) => (
           <StyledCategoryUl key={categoryIndex}>
@@ -46,9 +47,27 @@ function NavigationWiki({ setIsChanged, isChange }: INavigationWikiProps) {
           </StyledCategoryUl>
         ))}
       </StyledCategoryContainer>
-    </Container>
+    </StyledWikiContainer>
   );
 }
+
+const StyledWikiContainer = styled(Container) `
+  ${media.desktop_lg(`
+    width: 10rem;
+  `)}
+  ${media.tablet(`
+    width: 8rem;
+  `)}
+  ${media.tablet_680(`
+    width: 7rem;
+  `)}
+  ${media.tablet_625(`
+    width: 6rem;
+  `)}
+  ${media.mobile_430(`
+    width: 5rem;
+  `)}  
+`;
 
 const StyledCategoryContainer = styled.div`
   display: flex;
@@ -58,7 +77,7 @@ const StyledCategoryContainer = styled.div`
 const StyledCategoryUl = styled.ul`
   padding: 0;
   color: white;
-  margin-top: 2rem;
+  margin: 2rem 0 0 0.25rem;
 
   h1 {
     margin-bottom: 1rem;
@@ -68,7 +87,7 @@ const StyledCategoryUl = styled.ul`
     margin: 1rem;
     cursor: default;
   }
-
+  
   span:hover {
     cursor: pointer;
   }
@@ -76,6 +95,22 @@ const StyledCategoryUl = styled.ul`
     font-weight: 700;
     border-bottom: 2px solid #e2e8f0;
   }
+  
+  ${media.desktop_lg(`
+    font-size: 1rem;
+  `)}
+  ${media.tablet(`
+    font-size: 0.9rem;
+  `)}
+  ${media.tablet_680(`
+    font-size: 0.8rem;
+  `)}
+  ${media.tablet_625(`
+    font-size: 0.5rem;
+  `)}
+  ${media.mobile_430(`
+    font-size: 0.4rem;
+  `)}
 `;
 
 export default NavigationWiki;
