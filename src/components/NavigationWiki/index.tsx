cbc1fe48132b72e2bbd98a192eadd74be0706286
89882 <pgh123166@naver.com>
2023-09-21 16:32:54 +0900

    refactor: navigation wiki

--- src/components/NavigationWiki/index.tsx
+++ src/components/NavigationWiki/index.tsx
@@ -1,94 +1,50 @@
 import styled from 'styled-components';
 import { Container } from 'components/NavigationContainer';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { ROUTES } from 'constants/routes';
+import { CATEGORY } from 'constants/wiki';
 
-interface props {
+interface INavigationWikiProps {
   setIsChanged: React.Dispatch<React.SetStateAction<boolean>>;
   isChange: boolean;
 }
 
-function NavigationWiki({ setIsChanged, isChange }: props) {
+interface ICategoryInfo {
+  text: string;
+  pathName: string;
+}
+interface ICategory {
+  name: string;
+  info: ICategoryInfo[];
+}
+
+function NavigationWiki({ setIsChanged, isChange }: INavigationWikiProps) {
   const navigate = useNavigate();
   const searchParams = new URLSearchParams(useLocation().search);
   const selectedCategory = searchParams.get('category');
 
-  const initialCompanyInfo = [
-    { id: 1, text: '회사 내규', pathName: 'companyRule' },
-    { id: 2, text: '팀 소개', pathName: 'companyTeam' },
-    { id: 3, text: '조직도', pathName: 'companyOrganization' },
-  ];
-
-  const initialProjectInfo = [
-    { id: 1, text: '진행중인 프로젝트', pathName: 'projects' },
-    { id: 2, text: '예정된 프로젝트', pathName: 'projectsExpected' },
-    { id: 3, text: '완료된 프로젝트', pathName: 'projectsFinished' },
-  ];
-
-  const initialOnBoardingInfo = [
-    { id: 1, text: '신입사원 필독서', pathName: 'onBoardingDocs' },
-    { id: 2, text: '온보딩 문서', pathName: 'onBoardingSubjects' },
-  ];
-
-  const companyList = initialCompanyInfo.map((company) => (
-    <li
-      key={company.id}
-      className={
-        company.pathName === selectedCategory ? 'selected_category' : ''
-      }
-      onClick={() => {
-        navigate(`/wiki?category=${company.pathName}`);
-        setIsChanged(!isChange);
-      }}
-    >
-      {company.text}
-    </li>
-  ));
-
-  const projectList = initialProjectInfo.map((project) => (
-    <li
-      key={project.id}
-      className={
-        project.pathName === selectedCategory ? 'selected_category' : ''
-      }
-      onClick={() => {
-        navigate(`/wiki?category=${project.pathName}`);
-        setIsChanged(!isChange);
-      }}
-    >
-      {project.text}
-    </li>
-  ));
-
-  const onBoardingList = initialOnBoardingInfo.map((onBoarding) => (
-    <li
-      key={onBoarding.id}
-      className={
-        onBoarding.pathName === selectedCategory ? 'selected_category' : ''
-      }
-      onClick={() => {
-        navigate(`/wiki?category=${onBoarding.pathName}`);
-        setIsChanged(!isChange);
-      }}
-    >
-      {onBoarding.text}
-    </li>
-  ));
-
   return (
     <Container>
       <CategoryContainer>
-        <CategoryUl>
-          <h1>회사생활</h1>
-          {companyList}
-        </CategoryUl>
-        <CategoryUl>
-          <h1>프로젝트</h1>
-          {projectList}
-        </CategoryUl>
-        <CategoryUl>
-          <h1>온보딩</h1>
-          {onBoardingList}
-        </CategoryUl>
+        {CATEGORY.map((category: ICategory, categoryIndex) => (
+          <CategoryUl key={categoryIndex}>
+            <h1>회사생활</h1>
+            {category.info.map((info, infoIndex) => (
+              <li
+                key={infoIndex}
+                className={
+                  info.pathName === selectedCategory ? 'selected_category' : ''
+                }
+                onClick={() => {
+                  navigate(`${ROUTES.WIKI}?category=${info.pathName}`);
+                  setIsChanged(!isChange);
+                }}
+              >
+                {info.text}
+              </li>
+            ))}
+          </CategoryUl>
+        ))}
       </CategoryContainer>
     </Container>
   );
@@ -121,4 +77,3 @@ const CategoryUl = styled.ul`
 `;
 
 export default NavigationWiki;
-
---
+++ src/constants/wiki.ts
@@ -0,0 +1,25 @@
+export const CATEGORY = [
+  {
+    name: '회사생활',
+    info: [
+      { text: '회사 내규', pathName: 'companyRule' },
+      { text: '팀 소개', pathName: 'companyTeam' },
+      { text: '조직도', pathName: 'companyOrganization' },
+    ],
+  },
+  {
+    name: '프로젝트',
+    info: [
+      { text: '진행중인 프로젝트', pathName: 'projects' },
+      { text: '예정된 프로젝트', pathName: 'projectsExpected' },
+      { text: '완료된 프로젝트', pathName: 'projectsFinished' },
+    ],
+  },
+  {
+    name: '온보딩',
+    info: [
+      { text: '신입사원 필독서', pathName: 'onBoardingDocs' },
+      { text: '온보딩 문서', pathName: 'onBoardingSubjects' },
+    ],
+  },
+];
