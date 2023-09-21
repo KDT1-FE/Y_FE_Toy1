e2e29d1ddde30f9d9e6a3d19b261cb84cc22a614
Subin Jang <wkdtnqls0506@naver.com>
2023-09-21 15:45:54 +0900

    feat: img register component

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -2,11 +2,12 @@ import NavigationGallery from 'components/NavigationGallery';
 import UploadGalleryModal from 'components/UploadGalleryModal';
 import ReadGallery from 'components/ReadGallery';
 import styled from 'styled-components';
-import Loading from 'components/Common/Loading';
+import { media } from 'styles/media';
+import Loading from 'components/Common/LoadingImgRegister';
 import { useState } from 'react';
 
 function Gallery() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <>
       <StyledContainer>
@@ -14,7 +15,10 @@ function Gallery() {
         <StyledGalleryContainer>
           {isLoading ? (
             <>
-              이미지 등록 중<Loading></Loading>
+              <StyledLoadingContainer>
+                <Loading></Loading>
+                <StyledLoadingText>이미지 등록 중</StyledLoadingText>
+              </StyledLoadingContainer>
             </>
           ) : (
             <>
@@ -39,6 +43,35 @@ const StyledGalleryContainer = styled.div`
   height: 56rem;
   display: flex;
   flex-direction: column;
+`;
+
+const StyledLoadingContainer = styled.div`
+  // border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  ${media.desktop_lg(`
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+`)}
+  ${media.tablet_680(`
+  height: 35rem;
+`)}
+${media.mobile_430(`
+  height: 25rem;
+`)}
+`;
+
+const StyledLoadingText = styled.div`
+  position: absolute;
+  bottom: 45%;
+  ${media.tablet_680(`
+    bottom: 43%;
+`)}
+  ${media.mobile_430(`
+    bottom: 40%;
+`)}
 `;
 
 export default Gallery;
