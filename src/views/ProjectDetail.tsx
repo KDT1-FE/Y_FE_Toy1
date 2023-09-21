03026542cd5f45965f875e16c704906ca14a7c23
IAMISTP <hyeminpak@gmail.com>
2023-09-21 12:24:13 +0900

    project Detail complete

--- src/views/ProjectDetail.tsx
+++ src/views/ProjectDetail.tsx
@@ -19,25 +19,27 @@ const ProjectDetail = ():JSX.Element => {
   if(project.isSuccess){
     const {writeDate, content, title, status}:DataType = project.data.data() as DataType;
     return (
-      <div className='project__detail_container'>
-        <button className='btn' onClick={() => navigate('/project')}>목록</button>
-        <button className='btn' onClick={() => navigate(`/project/update/${id}`)}>수정</button>
-        <div>{writeDate.toDate().toLocaleString()}</div>
-        <div style={{display:'flex'}}>
-                        <div>
+      <div className='project__detail'>
+        <div className='project__detail-btns-box'>
+          <button className='btn' onClick={() => navigate('/project')}>목록</button>
+          <button className='btn' onClick={() => navigate(`/project/update/${id}`)}>수정</button>
+        </div>
+        <div className='project__detail-date-box'><span>작성일</span><span className='project__detail-date'>{writeDate.toDate().toLocaleString()}</span></div>
+        <div className='project__detail-status-box'>
+                        <div className='project__detail-statu-box'>
                             <input type='radio' id='expected' checked={status === 'expected' ? true : false} defaultValue={'예정'} name='status' readOnly />
                             <label htmlFor='expected'>예정</label>
                         </div>
-                        <div>
+                        <div className='project__detail-statu-box'>
                             <input type='radio' id='progress' checked={status === 'proceeding' ? true : false} defaultValue={'진행중'} name='status' readOnly/>
                             <label htmlFor='progress'>진행중</label>
                         </div>
-                        <div>
+                        <div className='project__detail-statu-box'>
                             <input type='radio' id='complete' checked={status === 'complete' ? true : false} defaultValue={'완료'} name='status' readOnly/>
                             <label htmlFor='complete'>완료</label>
                         </div>
                     </div>
-        <h2>{title}</h2>
+        <div className='project__detail-title'>{title}</div>
         <Viewer initialValue={content} />
       </div>
     )
