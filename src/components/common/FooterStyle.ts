e48fcb282a4173fd321db5f2d630ad469179fff7
seungyeon Lee <seungyeonlee@seungyeonui-MacBookPro-2.local>
2023-09-22 12:10:16 +0900

    Rename: fooder에서 footer로 파일명변경

--- src/components/common/FooterStyle.ts
+++ src/components/common/FooterStyle.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const FooterWrap = styled.div`
+  margin-top: 40px;
   background-color: var(--color-main);
-  padding: 20px 80px;
+  padding: 5px 80px;
   color: var(--color-white);
+  opacity:20%;
 `;
 
 export const FooterTop = styled.div`
@@ -15,11 +17,8 @@ export const FooterTop = styled.div`
 
 export const FooterTopLeft = styled.div`
   & > span {
-    font-size: 1.5rem;
-  }
-  & > span:first-child {
-    padding: 0 10px;
-    border: 0.5px solid var(--color-white);
+    font-size: 1rem;
+    letter-spacing: -0.05rem;
   }
 `;
 
@@ -27,17 +26,22 @@ export const FooterTopMid = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  & > p {
+  & > a > p {
     margin: 0 10px;
+    font-size: 0.8rem;
+    letter-spacing: -0.04rem;
+  }
+
+  & > a > p:hover {
+    border-bottom: 1px solid #fff;
   }
 `;
 
 export const FooterTopRight = styled.div`
   & > a {
     display: inline-block;
-    width: 40px;
-    height: 40px;
+    width: 30px;
+    height: 30px;
   }
 
   & img {
