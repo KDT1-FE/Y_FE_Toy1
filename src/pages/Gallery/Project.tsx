57929c1810794f65b52279609cb8d944c29d0e3f
seokmin <soekmin1998@naver.com>
2023-09-22 18:06:45 +0900

    feat: Add pagination

--- src/pages/Gallery/Project.tsx
+++ src/pages/Gallery/Project.tsx
@@ -20,6 +20,44 @@ export interface ProjectProps extends ProjectStateProps {
 const Project = ({ state }: ProjectStateProps) => {
   const [projectData, setProjectData] = useState<Array<ProjectProps>>();
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
+  const SIZE = 6;
+
+  const getPaginatedData = () => {
+    const startIndex = (currentPage - 1) * SIZE;
+    const endIndex = startIndex + SIZE;
+    return projectData?.slice(startIndex, endIndex);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (projectData?.length) {
+      if (currentPage < maxPage) {
+        setCurrentPage(currentPage + 1);
+      }
+    }
+  };
+  
+  const prevButtonStyle = {
+    backgroundColor: currentPage === 1 ? 'rgb(252,252,252)' : '#3267B1',
+    color: currentPage === 1 ? '#797979' : '',
+    cursor: currentPage === 1 ? 'unset' : 'pointer',
+  };
+
+  const nextButtonDisable = currentPage === maxPage || maxPage === 1;
+
+  const nextButtonStyle = {
+    backgroundColor: nextButtonDisable ? 'rgb(252,252,252)' : '#3267B1',
+    color: nextButtonDisable ? '#797979' : '',
+    cursor: nextButtonDisable ? 'unset' : 'pointer',
+  };
+
   const fetchProjectData = async () => {
     try {
       const filterStateQuery = query(collection(db, 'projectData'), where('state', '==', state));
@@ -32,6 +70,7 @@ const Project = ({ state }: ProjectStateProps) => {
         filteredData.push({ ...data, projectId } as ProjectProps);
       });
       setProjectData(filteredData);
+      setMaxPage(Math.ceil(filteredData.length / SIZE));
     } catch (error) {
       console.error('프로젝트 Data Fetch 에러 :', error);
     }
@@ -60,7 +99,7 @@ const Project = ({ state }: ProjectStateProps) => {
         {isLoading ? (
           <LoadingSpinner />
         ) : (
-          projectData?.map((item, index) => {
+          getPaginatedData()?.map((item, index) => {
             return (
               <ImageWrapper
                 key={index}
@@ -76,6 +115,16 @@ const Project = ({ state }: ProjectStateProps) => {
         )}
         {projectData?.length === 0 && <div>{projectState} 프로젝트가 없습니다.</div>}
       </ImageSection>
+      {projectData?.length !== 0 && (
+        <ButtonSection>
+          <Button onClick={goToPreviousPage} style={prevButtonStyle}>
+            이전
+          </Button>
+          <Button onClick={goToNextPage} style={nextButtonStyle}>
+            다음
+          </Button>
+        </ButtonSection>
+      )}
     </GalleryMainContainer>
   );
 };
@@ -100,12 +149,38 @@ export const BreadCrumb = styled.span`
 `;
 export const CategoryTitle = styled.h1`
   font-size: 32px;
-`
-
+`;
 
 export const ImageSection = styled.section`
   display: flex;
   flex-wrap: wrap;
   gap: 0 5%;
   margin-top: 30px;
+`;
+
+const ButtonSection = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
+
+const Button = styled.button`
+  font-family: 'Noto Sans KR';
+  width: 120px;
+  cursor: pointer;
+  height: 47px;
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  font-weight: 600;
+  color: rgb(252, 252, 252);
+  text-align: center;
+  line-height: 47px;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
