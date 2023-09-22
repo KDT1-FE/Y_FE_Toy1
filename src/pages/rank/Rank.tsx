2d7f7a392f47486ccd13ffe8ad443d06b2fabebb
Hojin <wkdghwls0623@gmail.com>
2023-09-22 11:28:38 +0900

    Feat: 랭킹 구현

--- src/pages/rank/Rank.tsx
+++ src/pages/rank/Rank.tsx
@@ -5,6 +5,7 @@ import { db } from "../../firebase";
 import { IsMobile } from "utils/mediaQuery";
 import { collection, query, orderBy, limit, getDocs } from "firebase/firestore";
 import Sidebar from "components/layout/Sidebar";
+
 interface UsersData {
   id: string;
   class: string;
@@ -19,8 +20,9 @@ interface IContainer {
 
 const Rank = () => {
   const [users, setUsers] = useState<UsersData[]>([]);
+  const [podiumUsers, setPodiumdUsers] = useState<UsersData[]>([]);
   const q = query(collection(db, "user"), orderBy("studyTime", "desc"), limit(100));
-  const defaultImageUrl = process.env.PUBLIC_URL + "/png/default.png";
+  const defaultImageUrl = process.env.PUBLIC_URL + "/png/class_default.png";
 
   let leftMargin = 200;
   let topMargin = 60;
@@ -58,25 +60,51 @@ const Rank = () => {
           };
         });
         setUsers(data);
-        console.log(users);
+        setPodiumdUsers([data[1], data[0], data[2]]);
       } catch (error) {
         console.error("데이터를 불러오는데 실패했습니다.", error);
       }
     };
-
     fetchUser();
   }, []);
-
   return (
     <>
       <Sidebar />
       <Container leftmargin={leftMargin} topmargin={topMargin}>
         <RankWrapper>
-          {users.map((usersData: UsersData, index: number) => (
+          <div className="userPodium">
+            {podiumUsers.map((usersData: UsersData, index: number) => (
+              <div className={`userBox__${index + 1}`} key={usersData.id}>
+                <img className="crown" src={process.env.PUBLIC_URL + `/png/user__${index + 1}.png`} alt="crown" />
+                <div className={`user__${index + 1}`}>
+                  <img
+                    className="user__image-podium"
+                    src={process.env.PUBLIC_URL + "/png/user_default.png"}
+                    alt="프로필"
+                  />
+                </div>
+                <div className="userList__detailBox">
+                  <div className="userList__nickName-podium">{usersData.nickName}</div>
+                  <div className="userList__studyTime">
+                    <div className="studyTime">{usersData.studyTime}분</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {users.slice(4).map((usersData: UsersData, index: number) => (
             <div className="userList" key={usersData.id}>
-              <div>
+              <div className="userList__rank">{index + 4}</div>
+              <img className="user__image" src={process.env.PUBLIC_URL + "/png/user_default.png"} alt="프로필" />
+              <div className="userList__nickName">{usersData.nickName}</div>
+              <div className="userList__studyTime">
+                <div className="studyTime">{usersData.studyTime}분</div>
+              </div>
+
+              <div className="usetList__emblem">
                 <img
-                  className="useList_class"
+                  className="useList__class"
                   src={
                     usersData.class !== undefined && usersData.class !== null
                       ? process.env.PUBLIC_URL + `/png/class_${usersData.class}.png`
@@ -86,10 +114,6 @@ const Rank = () => {
                 />
                 <div className="class">{classConverter(parseInt(usersData.class))}</div>
               </div>
-
-              <div className="userList__rank">{index + 1}</div>
-              <div className="userList__nickName">{usersData.nickName}</div>
-              <div className="userList__studyTime">{usersData.studyTime}분</div>
             </div>
           ))}
         </RankWrapper>
@@ -114,8 +138,8 @@ const RankWrapper = styled.div`
   text-align: center;
   flex-direction: column;
   border-radius: 5px;
-  margin-bottom: 50px;
-  margin-top: 16px;
+  margin: 70px;
+  background-color: beige;
 
   .userList {
     font-weight: 700;
@@ -129,11 +153,12 @@ const RankWrapper = styled.div`
   }
 
   .userList img {
-    height: 50px;
+    height: 100px;
   }
 
   .class {
     font-size: 12px;
+    margin-bottom: 10px;
   }
 
   .userList:hover {
@@ -146,11 +171,97 @@ const RankWrapper = styled.div`
   }
 
   .userList__nickName {
-    flex: 1 1 20%;
+    font-size: 25px;
+    flex: 1 1 40%;
   }
 
   .userList__studyTime {
-    flex: 1 1 70%;
+    flex: 1 1 50%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .usetList__emblem {
+    flex: 1 1 30%;
+  }
+
+  .studyTime {
+    width: 100px;
+    height: auto;
+    background-color: ${(props) => props.theme.body};
+    border-radius: 5%;
+    padding: 10px;
+    text-align: center;
+    font-size: 25px;
+    font-weight: 700;
+  }
+
+  .user__image {
+    width: 100px;
+    height: 100px;
+    border: 1px solid gray;
+    margin: 10px;
+  }
+
+  .userPodium {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    height: 450px;
+  }
+
+  .user__2 {
+    width: 150px;
+    height: 150px;
+    background-color: var(--main-color);
+    margin: 0 10px 10px 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .user__1 {
+    width: 150px;
+    height: 150px;
+    background-color: var(--main-color);
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .user__3 {
+    width: 150px;
+    height: 150px;
+    background-color: var(--main-color);
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .crown {
+    width: 100px;
+    height: auto;
+  }
+
+  .userBox__1 {
+    transform: translateY(50px);
+  }
+
+  .userBox__3 {
+    transform: translateY(70px);
+  }
+
+  .user__image-podium {
+    height: 100px;
+    width: 100px;
+  }
+
+  .userList__nickName-podium {
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 10px;
   }
 `;
 
